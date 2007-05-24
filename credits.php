2a7f41807bc2e53422ab1f66ce7ef925ee264fea
Rejo Zenger <rejo@zenger.nl>
2007-05-24 00:03:58 +0000

    Created a link to this versions homepage in credits.php.

--- credits.php
+++ credits.php
@@ -4,7 +4,7 @@ include_once("inc/header.inc.php");
 ?>
     <h2><? echo _('Credits'); ?></h2>
 
-    <p>This version is an adaption of the original <a href="http://www.poweradmin.org">Poweradmin</a>, version 1.2.7-patched. Poweradmin was written by <a href="http://sjeemz.nl/">Sjeemz</a> and <a href="http://www.trancer.nl/">Trancer</a>. The Poweradmin code includes patches by <a href="http://mostrey.be/">Wim Mostrey</a> and Dennis Roos. This version has been patched by <a href="http://rejo.zenger.nl">Rejo Zenger</a> and includes many additional features like multi-language support, an update of the database abstraction layer, support for slave zones, support for supermasters, basic support for skins and a number of bug fixes.</p>
+    <p><a href="https://rejo.zenger.nl/poweradmin/demo/">This version</a> is an adaption of the original <a href="http://www.poweradmin.org">Poweradmin</a>, version 1.2.7-patched. Poweradmin was written by <a href="http://sjeemz.nl/">Sjeemz</a> and <a href="http://www.trancer.nl/">Trancer</a>. The Poweradmin code includes patches by <a href="http://mostrey.be/">Wim Mostrey</a> and Dennis Roos. <a href="https://rejo.zenger.nl/poweradmin/demo/">This version</a> has been patched by <a href="http://rejo.zenger.nl">Rejo Zenger</a> and includes many additional features like multi-language support, an update of the database abstraction layer, support for slave zones, support for supermasters, basic support for skins and a number of bug fixes.</p>
     
 <?
 include_once("inc/footer.inc.php");
