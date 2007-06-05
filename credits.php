2fcf5ca9cb093d2370128a0a56980d403071782a
Rejo Zenger <rejo@zenger.nl>
2007-06-05 23:21:53 +0000

    Updated credits page.

--- credits.php
+++ credits.php
@@ -6,7 +6,7 @@ include_once("inc/header.inc.php");
 
     <p><a href="https://rejo.zenger.nl/poweradmin/">This version</a> is an adaption of the original <a href="http://www.poweradmin.org">Poweradmin</a>, version 1.2.7-patched. Poweradmin was written by <a href="http://sjeemz.nl/">Sjeemz</a> and <a href="http://www.trancer.nl/">Trancer</a>. The Poweradmin code includes patches by <a href="http://mostrey.be/">Wim Mostrey</a> and Dennis Roos. <a href="https://rejo.zenger.nl/poweradmin/">This version</a> has been patched by <a href="http://rejo.zenger.nl">Rejo Zenger</a> and includes many additional features like multi-language support, an update of the database abstraction layer, support for slave zones, support for supermasters, basic support for skins and a number of bug fixes.</p>
 
-    <p>Thank you Balazs Petrikovics for submitting bugs and patches.</p>
+    <p>Thanks to Balazs Petrikovics for submitting bugs and patches.</p>
 
 <?
 include_once("inc/footer.inc.php");
