3939e635dd68bdff997d39482386ef61d6a22f2a
Rejo Zenger <rejo@zenger.nl>
2007-06-25 20:52:41 +0000

    Improved printing of revision information in footer.

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,15 +1,18 @@
 <?
-
 global $db;
 if(is_object($db))
 {
 	 $db->disconnect();
 }
 
+ $svn_rev = '$LastChangedRevision$ $Date: 2006-07-22 21:42:37 -0700 (Sat, 22 Jul 2006) $ $Author$';
+ $svn_rev = preg_split("/[\s,]+/", $svn_rev);
+ $revision = "revision $svn_rev[1] (commited at $svn_rev[4] by $svn_rev[13])";
+
 ?>
   </div> <!-- /content -->
   <div class="footer">
-   <strong>poweradmin</strong> revision $LastChangedRevision$ (changed by $LastChangedBy$) - <a href="credits.php"><? echo _('credits'); ?></a>
+   <strong>poweradmin</strong> <? echo $revision;?> - <a href="credits.php"><? echo _('credits'); ?></a>
   </div>
 <?
 if(file_exists('inc/custom_footer.inc.php')) 
