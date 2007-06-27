613a201e2b82af08879a2bac524b13fa0552810d
Rejo Zenger <rejo@zenger.nl>
2007-06-27 21:33:53 +0000

    Removed version information in footer. Doesn't work. Isn't global.

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -5,14 +5,10 @@ if(is_object($db))
 	 $db->disconnect();
 }
 
- $svn_rev = '$LastChangedRevision$ $LastChangedDate$ $Author$';
- $svn_rev = preg_split("/[\s,]+/", $svn_rev);
- $revision = "revision $svn_rev[1] (commited at $svn_rev[4] by $svn_rev[13])";
-
 ?>
   </div> <!-- /content -->
   <div class="footer">
-   <strong>poweradmin</strong> <? echo $revision;?> - <a href="credits.php"><? echo _('credits'); ?></a>
+   <a href="https://rejo.zenger.nl/poweradmin/">a complete(r) <strong>poweradmin</strong></a>
   </div>
 <?
 if(file_exists('inc/custom_footer.inc.php')) 
