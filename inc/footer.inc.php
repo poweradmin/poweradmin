6da32bc051d7015e97fb520af4cbb04a9dda5d73
Rejo Zenger <rejo@zenger.nl>
2007-06-27 22:36:02 +0000

    Re-added link to credits (was removed by accident as well).

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -8,7 +8,7 @@ if(is_object($db))
 ?>
   </div> <!-- /content -->
   <div class="footer">
-   <a href="https://rejo.zenger.nl/poweradmin/">a complete(r) <strong>poweradmin</strong></a>
+   <a href="https://rejo.zenger.nl/poweradmin/">a complete(r) <strong>poweradmin</strong></a> - <a href="credits.php">credits</a>
   </div>
 <?
 if(file_exists('inc/custom_footer.inc.php')) 
