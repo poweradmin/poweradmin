ea806eb711d976c9abc97dce54f6f847a063d283
Peter Beernink <peter@wijzijnhet.nl>
2008-01-29 21:32:48 +0000

    Fixed a bug reported by Balazs Petrikovics. i18n doesn't work correctly on FreeBSD.

--- inc/i18n.inc.php
+++ inc/i18n.inc.php
@@ -26,5 +26,6 @@ setlocale(LC_ALL, $language);
 $gettext_domain = 'messages';
 bindtextdomain($gettext_domain, "./locale");
 textdomain($gettext_domain);
-
+@putenv('LANG='.$language);
+@putenv('LANGUAGE='.$language);
 ?>
