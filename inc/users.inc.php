3a4e06a98f8190a9595c9c7f3de5dd30bed96e5c
Rejo Zenger <rejo@zenger.nl>
2008-03-04 08:12:12 +0000

    Removing dot-comma's from recently created queries to avoid compatibility issues with non-MySQL databases.

--- inc/users.inc.php
+++ inc/users.inc.php
@@ -39,14 +39,14 @@ function verify_permission($permission) {
         // Find the template ID that this user has been assigned.
         $query = "SELECT perm_templ
 			FROM users 
-			WHERE id = " . $db->quote($userid) . ";";
+			WHERE id = " . $db->quote($userid) ;
         $templ_id = $db->queryOne($query);
 
         // Does this user have ueberuser rights?
         $query = "SELECT id 
 			FROM perm_templ_items 
 			WHERE templ_id = " . $db->quote($templ_id) . " 
-			AND perm_id = '30';";
+			AND perm_id = '30'";
         $result = $db->query($query);
         if ( $result->numRows() > 0 ) {
                 return 1;
@@ -55,14 +55,14 @@ function verify_permission($permission) {
         // Find the permission ID for the requested permission.
         $query = "SELECT id 
 			FROM perm_items 
-			WHERE name = " . $db->quote($permission) . ";";
+			WHERE name = " . $db->quote($permission) ;
         $perm_id = $db->queryOne($query);
 
         // Check if the permission ID is assigned to the template ID. 
         $query = "SELECT id 
 			FROM perm_templ_items 
 			WHERE templ_id = " . $db->quote($templ_id) . " 
-			AND perm_id = " . $db->quote($perm_id) . ";";
+			AND perm_id = " . $db->quote($perm_id) ;
         $result = $db->query($query);
         if ( $result->numRows() > 0 ) {
                 return 1;
