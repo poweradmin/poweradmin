7dadcdbec9c06337794c5a2e5a9c297c2d43e816
Rejo Zenger <rejo@zenger.nl>
2008-04-03 18:57:11 +0000

    Changed URL's to Poweradmin website in footer of PHP pages.

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -28,7 +28,7 @@ if(is_object($db))
 ?>
   </div> <!-- /content -->
   <div class="footer">
-   <a href="https://rejo.zenger.nl/poweradmin/">a complete(r) <strong>poweradmin</strong></a> - <a href="https://code.krikkit.nl/trac/poweradmin/wiki/Credits">credits</a>
+   <a href="https://www.poweradmin.org/">a complete(r) <strong>poweradmin</strong></a> - <a href="https://www.poweradmin.org/trac/wiki/Credits">credits</a>
   </div>
 <?php
 if(file_exists('inc/custom_footer.inc.php')) 
