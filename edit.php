079fbfcf994b5ef56c6433e4fa088e60f24bc855
Edmondas Girkantas <eg@fbsd.lt>
2010-11-04 18:43:59 +0000

    TTN #388 - show priority field for every record, so changing from one DNS RR type to other would be always possible.

--- edit.php
+++ edit.php
@@ -156,12 +156,9 @@ if ( $perm_view == "none" || $perm_view == "own" && $user_is_zone_owner == "0" )
 					echo "       </select>\n";
 					echo "      </td>\n";
 					echo "      <td class=\"u\"><input class=\"wide\" name=\"record[" . $r['id'] . "][content]\" value='" . $clean_content . "'></td>\n";
-					if ($r['type'] == "MX" || $r['type'] == "SRV") {
-						echo "      <td class=\"u\"><input name=\"record[" . $r['id'] . "][prio]\" value=\"" .  $r['prio'] . "\"></td>\n";
-					} else {
-						echo "      <td class=\"n\">&nbsp;</td>\n";
-					}
+					echo "      <td class=\"u\"><input id=\"priority_field_" . $r['id'] . "\" name=\"record[" . $r['id'] . "][prio]\" value=\"" .  $r['prio'] . "\"></td>\n";
 					echo "      <td class=\"u\"><input name=\"record[" . $r['id'] . "][ttl]\" value=\"" . $r['ttl'] . "\"></td>\n";
+					
 				}
 				echo "     </tr>\n";
 			}
