07d47302c4497e5ac32c564c1e0a7a4a99b4ebad
Edmondas Girkantas <eg@fbsd.lt>
2011-01-08 08:46:39 +0000

    added license information

--- inc/helper.js
+++ inc/helper.js
@@ -1,5 +1,25 @@
 <!--
 
+/*  Poweradmin, a friendly web-based admin tool for PowerDNS.
+ *  See <https://rejo.zenger.nl/poweradmin> for more details.
+ *
+ *  Copyright 2007-2010  Rejo Zenger <rejo@zenger.nl>
+ *  Copyright 2010-2011  Poweradmin Development Team <http://www.poweradmin.org/credits>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function changePort(db_type) {
 	var dbport = document.getElementById("dbport");
 	
@@ -9,8 +29,6 @@ function changePort(db_type) {
 		dbport.value = "5432";
 	}
 }
-
--->
 
 //Add more fields dynamically.
 function addField(area,field,limit) {
@@ -96,3 +114,6 @@ function checkDomainFilled(){
 
     add_zone_master.submit();
 }
+
+-->
+
