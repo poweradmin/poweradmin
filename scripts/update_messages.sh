10cc7194f8c80ce1e838c8a2d343632225f6d987
Edmondas Girkantas <eg@fbsd.lt>
2011-03-11 11:48:18 +0000

    removed unused variable, improved code style

--- scripts/update_messages.sh
+++ scripts/update_messages.sh
@@ -3,12 +3,14 @@
 # get list of available locales, excluding english
 dirs=`ls ../locale | grep -v en_EN`
 
-# update every messages.mo in every locale
+# update every messages.mo for every locale
 for locale in $dirs; do
 	echo "Updating $locale locale"
+
 	cd ../locale/$locale/LC_MESSAGES
-	files=`ls`
+
 	name=`echo $locale | cut -c1-2`
 	msgfmt $name.po -o messages.mo
+
 	cd ../../
 done 
