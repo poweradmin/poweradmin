286f52d1b7bcd1fa7eebf273d4fedec3e025c816
Edmondas Girkantas <eg@fbsd.lt>
2011-06-06 20:43:08 +0000

    Merged r512 (fixes for #412, #413, #416) from the trunk

--- inc/record.inc.php
+++ inc/record.inc.php
@@ -1276,14 +1276,15 @@ function update_zone_records($zone_id, $zone_template) {
                         }
 
                         if($zone_master_add == "1" || $zone_slave_add == "1") {
+                                $domain = get_zone_name_from_id($zone_id);
                                 $now = time();
                                 $templ_records = get_zone_templ_records($zone_template);
                                 foreach ($templ_records as $r) {
                                         if ((preg_match('/in-addr.arpa/i', $zone_id) && ($r["type"] == "NS" || $r["type"] == "SOA")) || (!preg_match('/in-addr.arpa/i', $zone_id)))
                                         {
-                                                $name     = parse_template_value($r["name"], $zone_id);
+                                                $name     = parse_template_value($r["name"], $domain);
                                                 $type     = $r["type"];
-                                                $content  = parse_template_value($r["content"], $zone_id);
+                                                $content  = parse_template_value($r["content"], $domain);
                                                 $ttl      = $r["ttl"];
                                                 $prio     = intval($r["prio"]);
 
@@ -1307,7 +1308,7 @@ function update_zone_records($zone_id, $zone_template) {
 
         $query = "UPDATE zones
                     SET zone_templ_id = " . $db->quote($zone_template, 'integer') . "
-                    WHERE id = " . $db->quote($zone_id, 'integer') ;
+                    WHERE domain_id = " . $db->quote($zone_id, 'integer') ;
 	$response = $db->exec($query);
 
         if (PEAR::isError($response)) {
--- inc/templates.inc.php
+++ inc/templates.inc.php
@@ -312,7 +312,7 @@ function get_zone_templ_is_owner($zone_templ_id, $userid) {
 
 // Add a zone template from zone / another template.
 
-function add_zone_templ_save_as($template_name, $description, $userid, $records) {
+function add_zone_templ_save_as($template_name, $description, $userid, $records, $domain = null) {
 	global $db;
 
 	if (!(verify_permission('zone_master_add'))) {
@@ -339,9 +339,12 @@ function add_zone_templ_save_as($template_name, $description, $userid, $records)
                             $content = $db->quote($record['content'], 'text');
                     }
 
+                    $name = $domain ? preg_replace('/'.$domain.'/', '[ZONE]',$record['name']) : $record['name'];
+                    $content = $domain ? preg_replace('/'.$domain.'/', '[ZONE]',$content) : $content;
+
                     $query2 = "INSERT INTO zone_templ_records (zone_templ_id, name, type, content, ttl, prio) VALUES ("
                                             . $db->quote($zone_templ_id, 'integer') . ","
-                                            . $db->quote($record['name'], 'text') . ","
+                                            . $db->quote($name, 'text') . ","
                                             . $db->quote($record['type'], 'text') . ","
                                             . $content . ","
                                             . $db->quote($record['ttl'], 'integer') . ","
