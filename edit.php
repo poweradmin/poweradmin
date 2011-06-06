ed3b6902802d6b02bea9530d46e2938a7ab20295
Edmondas Girkantas <eg@fbsd.lt>
2011-06-06 20:41:43 +0000

    Merged r510 (fix bug #411) from the trunk

--- edit.php
+++ edit.php
@@ -51,9 +51,15 @@ if (isset($_POST['commit'])) {
 }
 
 if (isset($_POST['save_as'])) {
+	if (zone_templ_name_exists($_POST['templ_name'])) {
+		error(ERR_ZONE_TEMPL_EXIST);
+	} elseif ($_POST['templ_name'] == '') {
+		error(ERR_ZONE_TEMPL_IS_EMPTY);
+	} else {
+		success(SUC_ZONE_TEMPL_ADD);
         $records = get_records_from_domain_id($zone_id);
         add_zone_templ_save_as($_POST['templ_name'], $_POST['templ_descr'], $_SESSION['userid'], $records);
-
+	}
 }
 
 /*
