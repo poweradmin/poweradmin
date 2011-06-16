6c5bfdf2b83bf2f2e9dd4ecec0cd04a26f0d15a5
Edmondas Girkantas <eg@fbsd.lt>
2011-06-16 17:57:56 +0000

    added phpunit configuration file, updated sample test, now it uses constants from config

--- tests/functional/TitleTest.php
+++ tests/functional/TitleTest.php
@@ -5,12 +5,11 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 class TitleTest extends PHPUnit_Extensions_SeleniumTestCase {
 
 	protected function setUp() {
-		$this->setBrowser('*firefox');
-		$this->setBrowserUrl('http://127.0.0.1/');
+		$this->setBrowserUrl(BROWSER_URL);
 	}
 
-	public function testLogin() {
-		$this->open("poweradmin/");
+	public function testTitle() {
+		$this->open(SERVER_PATH);
 		$this->assertTitle('Poweradmin');
 	} 
 } 
