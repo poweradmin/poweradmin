0275c464cf35219361fa2c5a93250ef4e0499452
Edmondas Girkantas <eg@fbsd.lt>
2011-06-27 20:07:40 +0000

    added user password changing test case

--- tests/functional/UserTest.php
+++ tests/functional/UserTest.php
@@ -9,19 +9,31 @@ class UserTest extends PHPUnit_Extensions_SeleniumTestCase {
 		$this->setBrowserUrl(BROWSER_URL);
 	}
 
-	public function testLogin() {
+	public function te2stLogin() {
 		Common::doLogin();
 
 		$this->verifyTextPresent("Welcome Administrator");
 	} 
 
-	public function testLogout() {
+	public function te1stLogout() {
 		Common::doLogin();
 
 		$this->click("link=Logout");
 		$this->waitForPageToLoad("30000");
 		$this->verifyTextPresent("You have logged out.");
 	}	
+
+	public function testChangePassword() {
+		Common::doLogin();
+		Common::doChangePassword('admin', 'nimda');
+
+		$this->verifyTextPresent("Password has been changed, please login.");
+
+		// restore default password
+		// FIXME: use database fixtures
+		Common::doLogin('nimda');
+		Common::doChangePassword('nimda', 'admin');
+	}
 
 } 
 
--- tests/functional/common.php
+++ tests/functional/common.php
@@ -4,14 +4,22 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 class Common extends PHPUnit_Extensions_SeleniumTestCase {
 
-	public function doLogin() {
+	public function doLogin($password='admin') {
 		$this->open(SERVER_PATH);
 		$this->type('username', 'admin');
-		$this->type('password', 'admin');
+		$this->type('password', $password);
 		$this->click('authenticate');
 		$this->waitForPageToLoad("30000");
 	}
 
+	public function doChangePassword($old, $new) {
+		$this->click("link=Change password");
+		$this->waitForPageToLoad("30000");
+		$this->type('currentpass', $old);
+		$this->type('newpass', $new);
+		$this->type('newpass2', $new);
+		$this->clickAndWait("submit");
+	}
 }
 
 ?>
