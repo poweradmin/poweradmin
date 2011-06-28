e75a67edd886e05132d117fde256b4d37b1dae6f
Edmondas Girkantas <eg@fbsd.lt>
2011-06-28 23:17:43 +0000

    allow to create release file from provided svn branch/tag

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,17 +1,91 @@
 #!/bin/sh
 
+# Example usages:
+# ./create_release.sh 			- uses current directory
+# ./create_release.sh tags 2.1.5	- uses selected svn tag 
+# ./create_release.sh branches smarty	- uses selected svn branch
+# ./create_release.sh trunk		- uses svn trunk 
+
 # files and directories to exclude
 EXCLUDE_FILES_DIRS=".todo tests scripts db phpunit.xml.dist config.inc.php \
 	test.sh .orig .rej .patch .diff .new"
 
-# TODO: check if there is parameter, then checkout from svn specified version
+WORK_DIR=".."
 
-# get release version
-VERSION=`cat ../inc/version.inc.php | grep VERSION | cut -d '"' -f2`
+# check command usage
+if [ "$0" != "./create_release.sh" ]
+then
+	echo "Usage: ./create_release [trunk|tags x.x.x|branches name]"
+fi
+
+# check if there is parameter, then checkout from svn specified version
+if [ $# -eq 1 ]
+then 
+	if [ "$1" != "trunk" ]
+	then
+		echo "Error: unknow svn branch <$1>, use trunk!"
+		exit
+	fi
+
+	WORK_DIR="trunk"
+	VERSION="trunk"
+
+	if [ -e $WORK_DIR ]
+	then
+		echo "Error: checkout directory <$WORK_DIR> already exists!"
+		exit
+	fi
+
+	svn co https://www.poweradmin.org/svn/$1 $WORK_DIR 
+
+	if [ $? -ne 0 ]
+	then
+		echo "Error: svn checkout failed!"
+		exit
+	fi
+fi
+
+if [ $# -eq 2 ]
+then
+	SVN_BRANCH=$1
+	SVN_PROJ_OR_TAG=$2
+
+	WORK_DIR=$SVN_BRANCH-$SVN_PROJ_OR_TAG
+	if [ "$SVN_BRANCH" = "branches" ]
+	then
+		IS_BUGFIX_REL=`echo $SVN_PROJ_OR_TAG | grep ^[0-9]`
+		if [ "$IS_BUGFIX_REL" = "" ]
+		then
+			VERSION=$SVN_PROJ_OR_TAG
+		fi
+	fi
+
+	if [ -e $WORK_DIR ]
+	then
+		echo "Error: checkout directory <$WORK_DIR> already exists!"
+		exit
+	fi
+
+	svn co https://www.poweradmin.org/svn/$SVN_BRANCH/$SVN_PROJ_OR_TAG \
+		$WORK_DIR 
+
+	if [ $? -ne 0 ]
+	then
+		echo "Error: svn checkout failed!"
+		exit
+	fi
+fi
+
+# get release version if current directory is used
 if [ "$VERSION" = "" ]
 then
-	echo "Error: couldn't get release version!"
-	exit
+	VERSION=`cat $WORK_DIR/inc/version.inc.php | grep VERSION | cut -d '"' -f2`
+
+	if [ "$VERSION" = "" ]
+	then
+		echo "Error: couldn't get release version!"
+		exit
+	fi
 fi
 
 # check if output file already exists
@@ -31,19 +105,19 @@ done
 
 # revert changes in header.inc.php file, minor changes for developer mode
 # ignore_install_dir = true)
-cp ../inc/header.inc.php ../inc/header.inc.php.orig
-svn revert ../inc/header.inc.php
+cp $WORK_DIR/inc/header.inc.php $WORK_DIR/inc/header.inc.php.orig
+svn revert $WORK_DIR/inc/header.inc.php
 
 # check if temporary release directory exists
 if [ -e $RELEASE_NAME ]
 then
 	echo "Error: output release directory <$RELEASE_NAME> already exists!"
-	mv ../inc/header.inc.php.orig ../inc/header.inc.php
+	mv $WORK_DIR/inc/header.inc.php.orig $WORK_DIR/inc/header.inc.php
 	exit
 fi
 
 # copy all files to new release directory
-cp -r ../ $RELEASE_NAME
+cp -r $WORK_DIR/ $RELEASE_NAME
 
 # create tgz archive, exclude some directories & files
 tar cvzf $RELEASE_FILE $RELEASE_NAME --exclude=$RELEASE_FILE --exclude-vcs $OTHER_EXCLUDES
@@ -52,7 +126,7 @@ then
 	echo "Error: creation of release failed!"
 
 	# restore previous file system state
-	mv ../inc/header.inc.php.orig ../inc/header.inc.php
+	mv $WORK_DIR/inc/header.inc.php.orig $WORK_DIR/inc/header.inc.php
 	rm -rf $RELEASE_NAME
 
 	exit
@@ -70,5 +144,8 @@ fi
 rm -rf $RELEASE_NAME
 
 # restore header.inc.php if developer mode was enabled
-mv ../inc/header.inc.php.orig ../inc/header.inc.php
+if [ -e $WORK_DIR"/inc/header.inc.php.orig" ]
+then
+	mv $WORK_DIR/inc/header.inc.php.orig $WORK_DIR/inc/header.inc.php
+fi
 
