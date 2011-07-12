e830ec8b3380cc0a9914434c36f43033ad6c6a39
Edmondas Girkantas <eg@fbsd.lt>
2011-07-12 20:52:53 +0000

    applied patch from TTN #450, made source code & functionality cleanup

--- addons/dynamic_dns_client.pl
+++ addons/dynamic_dns_client.pl
@@ -1,39 +1,49 @@
-#!/usr/bin/perl -w
-use strict;
-use warnings;
+#!/usr/bin/env perl
+
 use LWP::Simple;
 use Socket;
 
+use strict;
+use warnings;
 
+# change these values
+#my $login    = 'username';
+my $login    = 'admin';
+#my $password = 'password';
+my $password = 'admin';
+my $domain   = 'mydynamicdns.example.com';
 
-# set these four up#
+my $verbose = 1;
 
-my $login = 'usernameforzoneadmin';
-my $password = 'passwordforzoneadmin';
-my $domain = 'remoteofficecableinternet.powerdns.example.com';
-my $poweradmin = 'powerdns.example.com';
+#my $poweradmin_url = 'http://example.com/poweradmin/';
+my $poweradmin_url = 'http://127.0.0.1/poweradmin/';
 
-#####################
-
-
-
-my $ipaddress = LWP::Simple::get("http://www.whatismyip.com");
-if ($ipaddress =~ /([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/) 
-{ 
-  $ipaddress = $1; 
+# try to get client ip address using whatismyip service
+#my $ipaddress =
+#  LWP::Simple::get("http://automation.whatismyip.com/n09230945.asp")
+#  or die("Error: Could not get your global IP address!\n");
+my $ipaddress = '127.0.0.1';
+if ( $ipaddress =~ /([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/ ) {
+    $ipaddress = $1;
+}
+else {
+    print "Error: Could not get your global IP address!\n";
+    exit;
 }
 
 print "Updating the IP address ($ipaddress) now ... \n";
 
+# TODO: how it will work with https?
+# insert authentication data to url
+$poweradmin_url =~ s/^http:\/\//http:\/\/$login:$password\@/;
+my $response =
+  LWP::Simple::get( "$poweradmin_url/dynamic_update.php"
+      . "?hostname=$domain&myip=$ipaddress&verbose=1" )
+  or die($!);
 
-my $response = LWP::Simple::get("http://$login:$password\@" .
-"$poweradmin/dynamic_update.php?hostname=$domain" .
-"&myip=$ipaddress&verbose=1");
-if (!defined $response || $response eq "")
-{
-print "Status : Could not contact your poweradmin web server\n";
-exit(0);
+if ( !defined $response || $response eq "" ) {
+    print "Error: Could not contact your poweradmin web server\n";
+    exit(0);
 }
 
-print "Status : $response\n";
-
+print "Status: $response\n";
--- dynamic_update.php
+++ dynamic_update.php
@@ -1,4 +1,5 @@
 <?php
+
 /*  Poweradmin, a friendly web-based admin tool for PowerDNS.
  *  See <https://www.poweradmin.org> for more details.
  *
@@ -19,45 +20,41 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
-Necessary includes
-*/
 require('inc/config.inc.php');
 require('inc/database.inc.php');
+
 $db = dbConnect();
 
-/*
-Make query safe
-*/
+// make sql query safe
 function safe($value) {
         $value = mysql_real_escape_string($value);
         return $value;
 }
 
-/*
-Exit status
-*/
+// get exit status message
 function status_exit($status) {
         $verbose_codes = array(
-                'badagent'=>'Your user agent is not valid.',
-                'badauth'=>'Invalid username or password.  Authentication failed.',
-                'notfqdn'=>'The hostname you specified was not valid.',
-                'dnserr'=>'A DNS error has occurred on our end.  We apologize for any inconvenience.',
-                '!yours'=>'The specified hostname does not belong to you.',
-                'nohost'=>'The specified hostname does not exist.',
-                'good'=>'Your hostname has been updated.',
-                '911'=>'A critical error has occurred on our end.  We apologize for any inconvenience.',
-                'nochg'=>'This update was identical to your last update, so no changes were made to your hostname configuration.'
+                'badagent' => 'Your user agent is not valid.',
+                'badauth'  => 'Invalid username or password.  Authentication failed.',
+                'notfqdn'  => 'The hostname you specified was not valid.',
+                'dnserr'   => 'A DNS error has occurred on our end.  We apologize for any inconvenience.',
+                '!yours'   => 'The specified hostname does not belong to you.',
+                'nohost'   => 'The specified hostname does not exist.',
+                'good'     => 'Your hostname has been updated.',
+                '911'      => 'A critical error has occurred on our end.  We apologize for any inconvenience.',
+                'nochg'    => 'This update was identical to your last update, so no changes were made to your hostname configuration.'
         );
-        if ($_REQUEST['verbose']) {
-                list($code,$msg) = explode(' ',$status,2);
-                $status = $verbose_codes[$code] . ($msg?' '.$msg:'');
+
+        if (isset($_REQUEST['verbose'])) {
+		$pieces = split(' ', $status);
+                $status = $verbose_codes[$pieces[0]];
         }
         echo "$status\n";
         return false;
 }
 
-if (!$_SERVER['HTTP_USER_AGENT']) return status_exit('badagent');
+if (!(isset($_SERVER)) && !$_SERVER['HTTP_USER_AGENT']) return status_exit('badagent');
+
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
         header('WWW-Authenticate: Basic realm="DNS Update"');
         header('HTTP/1.0 401 Unauthorized');
@@ -68,40 +65,37 @@ $username = safe($_SERVER['PHP_AUTH_USER']);
 $password = md5(safe($_SERVER['PHP_AUTH_PW']));
 $hostname = safe($_REQUEST['hostname']);
 $ip = safe($_REQUEST['myip']);
+
 if (!preg_match('/^((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/i',$ip)) {
         return status_exit('dnserr');
 }
 
 if (!strlen($hostname)) return status_exit('notfqdn');
-/*
-Don't allow super user to be used to update
-*/
-if ($username == 'admin') return status_exit('badauth');
 
-$query = mysql_query("SELECT * FROM users WHERE username='$username' and password='$password'");
-$queryusers = $db->queryOne($query);
+// Don't allow super user to be used for update
+#if ($username == 'admin') return status_exit('badauth');
 
-//$userdetails = mysql_fetch_array($queryusers, MYSQL_ASSOC);
-$userdetails = $queryusers->fetchRow();
+$query = "SELECT * FROM users WHERE username='$username' and password='$password'";
+$userdetails = $db->queryRow($query);
 
-$querydomains = "SELECT domain_id FROM zones WHERE owner='{$userdetails["id"]}'";
-$querydomains = $db->query($querydomains);
+$query2 = "SELECT domain_id FROM zones WHERE owner='{$userdetails["id"]}'";
+$querydomains = $db->query($query2);
 $domainunauth = $querydomains->numRows();
 
-while ($row = $querydomains->fetchRow) {
+while ($row = $querydomains->fetchRow()) {
+	$query3 = "SELECT name FROM records WHERE domain_id='{$row["domain_id"]}' and type = 'A'";
+	$result = $db->query($query3);
 
-        $hostname = "SELECT name FROM records WHERE domain_id='{$row["domain_id"]}' and type = 'A'";
-	$queryhostname = $hostname->query();
-        while ($row2 = $queryhostname->fetchRow) {
-                if($hostname == $row2['name']){
-                $updatequery ="UPDATE records SET content ='{$ip}' where domain_id='{$row["domain_id"]}' and type='A'";
-		$query = $db->query($updatequery);
-                $domainunauth = "-1";
-                }
-        }
+	while ($row2 = $result->fetchRow()) {
+		if ($hostname == $row2['name']){
+			$updatequery ="UPDATE records SET content ='{$ip}' where domain_id='{$row["domain_id"]}' and type='A'";
+			$query = $db->query($updatequery);
+			$domainunauth = "-1";
+		}
+	}
 }
 
-if($domainunauth<0) return status_exit('good');
+if ($domainunauth < 0) return status_exit('good');
 
 return status_exit('!yours');
 
