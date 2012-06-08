e8b99b9501935fd3bde07a549a47b9fe5692bb10
Edmondas Girkantas <eg@fbsd.lt>
2012-06-08 23:24:09 +0300

    introduced new configuration variable dnssec_use, which by default will be disabled until this feature will be completely done and tested

--- inc/config-me.inc.php
+++ inc/config-me.inc.php
@@ -55,5 +55,7 @@ $syslog_ident = 'poweradmin';
 // On Windows usually only LOG_USER is available
 $syslog_facility = LOG_USER;
 
+// PowerDNSSEC settings
+$pdnssec_use = false;
 $pdnssec_command = '/usr/bin/pdnssec';
 ?>
