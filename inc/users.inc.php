da35349c0693413cc354e21dbfc682c45d588652
John <john@ibiblio.org>
2012-12-07 09:38:01 -0500

    Remove duplicate list_permission_templates from users.inc.php

--- inc/users.inc.php
+++ inc/users.inc.php
@@ -89,7 +89,7 @@ function verify_permission($permission)
     return ($db->queryOne($query) ? true : false);
 }
 
-/** List Permission Templates
+/** Get a list of all available permission templates
  *
  * @return mixed[] array of templates [id, name, descr]
  */
@@ -638,29 +638,6 @@ function get_permission_template_details($templ_id) {
 	$details = $response->fetchRow(); 
 	return $details;
 }	
-
-
-/** Get list of all permission templates
- *
- * @return mixed[] array of permission template details [id,name,descr]
- */
-function get_list_permission_templates() {
-	global $db;
-
-	$query = "SELECT * FROM perm_templ ORDER BY name";
-	$response = $db->query($query);
-	if (PEAR::isError($response)) { error($response->getMessage()); return false; }
-
-	$perm_templ_list = array();
-	while ($perm_templ = $response->fetchRow()) {
-		$perm_templ_list[] = array(
-			"id"	=>	$perm_templ['id'],
-			"name"	=>	$perm_templ['name'],
-			"descr"	=>	$perm_templ['descr']
-			);
-	}
-	return $perm_templ_list;
-}
 
 
 /** Add a Permission Template
