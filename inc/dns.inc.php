5a41160285524556df76cb17986b8835cc29b5c4
Ryan Petris <ryan@petris.tel>
2013-09-02 20:09:21 -0400

    * Don't validate CURL, MBOXFW, and NAPTR records as SPF records. * Remove unnecessary comment about validating a record as SPF. * Move validation for LOC records above the list of unvalidated records.

--- inc/dns.inc.php
+++ inc/dns.inc.php
@@ -114,24 +114,23 @@ function validate_input($rid, $zid, $type, &$content, &$name, &$prio, &$ttl) {
 			if (!is_valid_printable($content)) return false;
 			break;
 
-		case "CURL":
-		case "MBOXFW":
-		case "NAPTR":
 		case "SPF":
-			/*
-			Validate SPF entry
-			*/
-                        if(!is_valid_spf($content)) return false;
-		case "SSHFP":
-		case "URL":
-			// These types are supported by PowerDNS, but there is not
-			// yet code for validation. Validation needs to be added
-			// for these types. One Day Real Soon Now. [tm]
+			if(!is_valid_spf($content)) return false; 
 			break;
 
 		case "LOC":
 			if (!is_valid_loc($content)) return false;
 			if (!is_valid_hostname_fqdn($name,1)) return false;
+			break;
+			
+		case "CURL":
+		case "MBOXFW":
+		case "NAPTR":
+		case "SSHFP":
+		case "URL":
+			// These types are supported by PowerDNS, but there is not
+			// yet code for validation. Validation needs to be added
+			// for these types. One Day Real Soon Now. [tm]
 			break;
 
 		default:
