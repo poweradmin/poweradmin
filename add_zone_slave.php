bef67b9d374d32a23e7f338f7b81f0cdd2eaa287
Edmondas Girkantas <eg@fbsd.lt>
2014-04-05 11:25:59 +0300

    Fixed issues described in #111.

--- add_zone_slave.php
+++ add_zone_slave.php
@@ -60,7 +60,7 @@ if (isset($_POST['submit']) && $zone_slave_add == "1") {
         error(ERR_DNS_HOSTNAME);
     } elseif (domain_exists($zone)) {
         error(ERR_DOMAIN_EXISTS);
-    } elseif (!is_valid_ipv4($master) && !is_valid_ipv6($master)) {
+    } elseif (!is_valid_ipv4($master, false) && !is_valid_ipv6($master)) {
         error(ERR_DNS_IP);
     } else {
         if (add_domain($zone, $owner, $type, $master, 'none')) {
