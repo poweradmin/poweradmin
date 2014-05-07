fc22fc2fdd6736dbcd274ea44c352364538df23b
Edmondas Girkantas <eg@fbsd.lt>
2014-05-07 19:23:28 +0300

    Allow to select whether to secure zone or not.

--- add_zone_master.php
+++ add_zone_master.php
@@ -65,6 +65,11 @@ if (isset($_POST['zone_template'])) {
     $zone_template = "none";
 }
 
+$enable_dnssec = false;
+if (isset($_POST['dnssec']) && $_POST['dnssec'] == '1') {
+    $enable_dnssec = true;
+}
+
 /*
   Check user permissions
  */
@@ -89,7 +94,7 @@ if (isset($_POST['submit']) && $zone_master_add == "1") {
                               $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
                               $domain,$dom_type,$zone_template));
 
-            if ($pdnssec_use) {
+            if ($pdnssec_use && $enable_dnssec) {
                 dnssec_secure_zone($domain);
             }
         }
@@ -163,6 +168,10 @@ if ($zone_master_add != "1") {
     echo "         </select>\n";
     echo "        </td>\n";
     echo "        <td>&nbsp;</td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td class=\"n\">" . _('DNSSEC') . ":</td>\n";
+    echo "        <td class=\"n\"><input type=\"checkbox\" class=\"input\" name=\"dnssec\" value=\"1\"></td>\n";
     echo "       </tr>\n";
     echo "       <tr>\n";
     echo "        <td class=\"n\">&nbsp;</td>\n";
