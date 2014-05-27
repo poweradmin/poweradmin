782c14e31475754637b709e1486c59e8099912fc
Henk Loke <henk@loke.name>
2014-05-27 18:14:00 +0200

    : tests
    Task-Url:

--- inc/auth.inc.php
+++ inc/auth.inc.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< Upstream, based on branch 'modular_auth' of https://github.com/henkloke/poweradmin.git
 /*  Poweradmin, a friendly web-based admin tool for PowerDNS.
  *  See <http://www.poweradmin.org> for more details.
  *
@@ -11,21 +10,10 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
-=======
 /*
  * Only the first function gets called, soput the preferred function 
  * first
  * 
- * @
- * 
- */
-
-/*
- * Only the first function gets called, so put the preferred function
- * first
->>>>>>> 0263780 Merged
- *
- *
  */
 
 add_listener('authenticate', 'authenticate_local');
