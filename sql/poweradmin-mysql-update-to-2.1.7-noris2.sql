33a865b0f03bed7035f719e4109947620d41a257
Jonas Gröger <jonas.groeger@noris.de>
2015-08-04 10:33:48 +0200

    Add expired flag to RFCs

--- sql/poweradmin-mysql-update-to-2.1.7-noris2.sql
+++ sql/poweradmin-mysql-update-to-2.1.7-noris2.sql
@@ -18,19 +18,20 @@ CREATE TABLE IF NOT EXISTS `rfcs` (
   `id`        INT         NOT NULL AUTO_INCREMENT,
   `timestamp` DATETIME    NOT NULL,
   `initiator` VARCHAR(64) NOT NULL,
+  `expired`   BOOLEAN               DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # The actual data for RFCs (shadow version of `records`)
 CREATE TABLE IF NOT EXISTS `rfc_data` (
   `id`          INT           NOT NULL AUTO_INCREMENT,
-  `domain_id`   VARCHAR(45)       NULL,
-  `name`        VARCHAR(255)      NULL,
-  `type`        VARCHAR(10)       NULL,
-  `content`     VARCHAR(64000)    NULL,
-  `ttl`         INT(11)           NULL,
-  `prio`        INT(11)           NULL,
-  `change_date` INT(11)           NULL,
+  `domain_id`   VARCHAR(45),
+  `name`        VARCHAR(255),
+  `type`        VARCHAR(10),
+  `content`     VARCHAR(64000),
+  `ttl`         INT(11),
+  `prio`        INT(11),
+  `change_date` INT(11),
   `rfc` INT(11)               NOT NULL,
   PRIMARY KEY (`id`, `rfc`),
 
