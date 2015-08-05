b04890600642b34d8009cde8f257474b897bce18
Jonas Gröger <jonas.groeger@noris.de>
2015-08-05 15:02:21 +0200

    Add RfcChange constructor and fields

--- inc/RfcChange.class.php
+++ inc/RfcChange.class.php
@@ -1,15 +1,24 @@
 <?php
 
-class RfcChange {
+class RfcChange
+{
+    private $zone;
+    private $serial;
+    private $prior;
+    private $after;
 
     /**
      * RfcChange constructor.
-     * @param int $zone
-     * @param string $serial
-     * @param Record $prior
-     * @param Record $after
+     * @param int $zone The id of the zone / domain
+     * @param string $serial The serial based on which the change is valid.
+     * @param Record $prior The record before the change.
+     * @param Record $after The record after the change.
      */
-    public function __construct($zone, $serial, $prior, $after) {
-
+    public function __construct($zone, $serial, $prior, $after)
+    {
+        $this->zone = $zone;
+        $this->serial = $serial;
+        $this->prior = $prior;
+        $this->after = $after;
     }
 }
