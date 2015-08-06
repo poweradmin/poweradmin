eff9f27d1a3de696e78e11d3c136290a16272035
Jonas Gröger <jonas.groeger@noris.de>
2015-08-06 15:30:18 +0200

    Made upgrade scripts simpler

--- sql/poweradmin-mysql-downgrade-to-2.1.7.sql
+++ sql/poweradmin-mysql-downgrade-to-2.1.7.sql
@@ -1,9 +1,17 @@
 START TRANSACTION;
 
+# Logging
 DROP TABLE IF EXISTS `log_domains`;
 DROP TABLE IF EXISTS `log_domains_type`;
 DROP TABLE IF EXISTS `log_records`;
 DROP TABLE IF EXISTS `log_records_type`;
 DROP TABLE IF EXISTS `log_records_data`;
+
+# RFCs
+DELETE FROM `perm_items` WHERE `name` = 'zone_content_rfc_own' LIMIT 1;
+DELETE FROM `perm_items` WHERE `name` = 'zone_content_rfc_other' LIMIT 1;
+DROP TABLE IF EXISTS `rfc_change`;
+DROP TABLE IF EXISTS `rfc`;
+DROP TABLE IF EXISTS `rfc_data`;
 
 COMMIT;
