d6c01c75766b13564c0d2440f2cc6a3c6d17613a
Jonas Gröger <jonas.groeger@noris.de>
2015-08-20 14:23:55 +0200

    Permissions - DRY: Factor out view/edit/edit_meta

--- inc/Permissions.class.php
+++ inc/Permissions.class.php
@@ -69,6 +69,18 @@ class Permissions
         return self::boolean_hook(self::$VERIFY_PERMISSION, 'zone_content_edit_own_as_client');
     }
 
+    public static function can_edit_own_meta()
+    {
+        self::assert_user_is_available();
+        return self::boolean_hook(self::$VERIFY_PERMISSION, 'zone_meta_edit_own');
+    }
+
+    public static function can_edit_other_meta()
+    {
+        self::assert_user_is_available();
+        return self::boolean_hook(self::$VERIFY_PERMISSION, 'zone_meta_edit_others');
+    }
+
     ###########################################################################
     # PERMISSIONS WITH MORE THAN 2 RESULT STATES
 
@@ -82,6 +94,32 @@ class Permissions
             return ContentEditPermissions::OWN_AS_CLIENT;
         }
         return ContentEditPermissions::NONE;
+    }
+
+    ###########################################################################
+    # Legacy permission functions factored out.
+    # TODO 5: Use these functions instead of the if else mess everywhere
+
+    public static function get_perm_view_legacy()
+    {
+        if(self::can_view_other_zone()) { return 'all'; };
+        if(self::can_view_own_zone()) { return 'own'; }
+        return 'none';
+    }
+
+    public static function get_perm_content_edit_legacy()
+    {
+        if(self::can_edit_other_content()) { return 'all'; }
+        if(self::can_edit_own_content()) { return 'own'; }
+        if(self::can_edit_own_content_as_client()) { return 'own_as_client'; }
+        return 'none';
+    }
+
+    public static function get_perm_meta_edit_legacy()
+    {
+        if(self::can_edit_other_meta()) { return 'all'; }
+        if(self::can_edit_own_meta()) { return 'own'; }
+        return 'none';
     }
 
     ###########################################################################
