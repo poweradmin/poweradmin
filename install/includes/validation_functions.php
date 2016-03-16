d18acae6fe776d424bc766a54b77e40e1f837195
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2016-03-16 12:27:32 +0100

    Simplified code for isFile validation-function

--- install/includes/validation_functions.php
+++ install/includes/validation_functions.php
@@ -3,11 +3,4 @@
 /**
  * Custom validation-rule for checking if value is a valid file
  */
-Valitron\Validator::addRule('isFile', function($field, $value) {
-    // Checks if value is file
-    if (is_file($value)) {
-        return true;
-    }
-
-    return false;
-}, _('is not a file!'));
+Valitron\Validator::addRule('isFile', function($field, $value) { return is_file($value); }, _('is not a file!'));
