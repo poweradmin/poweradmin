f7e3bb2cf573947402081336ee0f846fef1cb05b
Jeroen Boonstra <jeroen@provider.nl>
2017-01-16 13:10:37 +0100

    Updated copyright to 2017

--- dynamic_update.php
+++ dynamic_update.php
@@ -4,7 +4,7 @@
  *  See <http://www.poweradmin.org> for more details.
  *
  *  Copyright 2007-2010  Rejo Zenger <rejo@zenger.nl>
- *  Copyright 2010-2014  Poweradmin Development Team
+ *  Copyright 2010-2017  Poweradmin Development Team
  *      <http://www.poweradmin.org/credits.html>
  *
  *  This program is free software: you can redistribute it and/or modify
