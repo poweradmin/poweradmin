e8ac27108d91463696ff97446c9844613481a6a8
Bill Hammond <bill.s.hammond@gmail.com>
2017-12-23 07:21:49 -0600

    Cleaned Up Text

--- edit_zone_templ.php
+++ edit_zone_templ.php
@@ -154,7 +154,7 @@ if (!(do_hook('verify_permission' , 'zone_master_add' )) || !$owner) {
             echo "&nbsp;&nbsp;&nbsp;&nbsp; * [SERIAL] - " . _('substituted with current date and 2 numbers') . " (YYYYMMDD + 00)\n <br>";
             echo "&nbsp;&nbsp;&nbsp;&nbsp; When using zone templates the SOA record will get added automatically to the new zone.<br>";
             echo "&nbsp;&nbsp;&nbsp;&nbsp; Examples: to add a subdomain foo in a zonetemplate you would put foo.[ZONE] into the name field. <br>";
-           	echo "&nbsp;&nbsp;&nbsp;&nbsp; To add a wildcard record put *.[ZONE] in the name field.<br>";
+            echo "&nbsp;&nbsp;&nbsp;&nbsp; To add a wildcard record put *.[ZONE] in the name field.<br>";
             echo "&nbsp;&nbsp;&nbsp;&nbsp; Use just [ZONE] to have the domain itself return a value.</td>";
             echo "     </tr>\n";
             echo "     <tr>\n";
