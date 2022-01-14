75e002cba6f6ba747b1dc7ea9aa4495f46de123c
Edmondas Girkantas <edmondas@girkantas.lt>
2022-01-14 08:27:41 +0200

    DNSSEC tool renamed to pdnsutil.

--- inc/config-me.inc.php
+++ inc/config-me.inc.php
@@ -67,7 +67,7 @@ $syslog_facility = LOG_USER;
 
 // PowerDNSSEC settings
 $pdnssec_use = false;
-$pdnssec_command = '/usr/bin/pdnssec';
+$pdnssec_command = '/usr/bin/pdnsutil';
 
 // LDAP settings
 $ldap_use = false;
