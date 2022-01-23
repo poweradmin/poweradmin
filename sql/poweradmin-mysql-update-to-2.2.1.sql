32597bb996955f97481f3bf85bd03823300c043b
Edmondas Girkantas <edmondas@girkantas.lt>
2022-01-23 18:18:45 +0200

    Fixed migration file.

--- sql/poweradmin-mysql-update-to-2.2.1.sql
+++ sql/poweradmin-mysql-update-to-2.2.1.sql
@@ -1,6 +1,6 @@
-ALTER TABLE perm_templ_items MODIFY templ_id integer(11);
-ALTER TABLE perm_templ_items MODIFY perm_id integer(11);
-ALTER TABLE zones MODIFY id integer(11);
-ALTER TABLE zones MODIFY domain_id integer(11);
-ALTER TABLE zones MODIFY owner integer(11);
-ALTER TABLE zones MODIFY zone_templ_id integer(11);
+ALTER TABLE perm_templ_items MODIFY templ_id int(11) NOT NULL;
+ALTER TABLE perm_templ_items MODIFY perm_id  int(11) NOT NULL;
+ALTER TABLE zones MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE zones MODIFY domain_id int(11) NOT NULL;
+ALTER TABLE zones MODIFY owner int(11) NOT NULL;
+ALTER TABLE zones MODIFY zone_templ_id int(11) NOT NULL;
