aa02c56e95dc7bc175ca8127f761394adc63a4aa
Edmondas Girkantas <edmondas@girkantas.lt>
2022-02-05 15:38:32 +0200

    Hooks moved to toolkit for future refactoring.

--- inc/toolkit.inc.php
+++ inc/toolkit.inc.php
@@ -47,7 +47,25 @@ require_once 'i18n.inc.php';
 // Checks if user is logging in, logging out, or session expired and performs actions accordingly
 add_listener('authenticate', 'authenticate_local');
 
-require_once 'users.inc.php';
+add_listener('verify_permission', 'verify_permission_local');
+add_listener('show_users', 'show_users_local');
+add_listener('change_user_pass', 'change_user_pass_local');
+add_listener('list_permission_templates', 'list_permission_templates_local');
+add_listener('is_valid_user', 'is_valid_user_local');
+add_listener('delete_user', 'delete_user_local');
+add_listener('delete_perm_templ', 'delete_perm_templ_local');
+add_listener('edit_user', 'edit_user_local');
+add_listener('get_fullname_from_userid', 'get_fullname_from_userid_local');
+add_listener('get_owner_from_id', 'get_owner_from_id_local');
+add_listener('get_fullnames_owners_from_domainid', 'get_fullnames_owners_from_domainid_local');
+add_listener('verify_user_is_owner_zoneid', 'verify_user_is_owner_zoneid_local');
+add_listener('get_user_detail_list', 'get_user_detail_list_local');
+add_listener('get_permissions_by_template_id', 'get_permissions_by_template_id_local');
+add_listener('get_permission_template_details', 'get_permission_template_details_local');
+add_listener('add_perm_templ', 'add_perm_templ_local');
+add_listener('update_perm_templ_details', 'update_perm_templ_details_local');
+add_listener('update_user_details', 'update_user_details_local');
+add_listener('add_new_user', 'add_new_user_local');
 
 $db = dbConnect();
 require_once 'dns.inc.php';
