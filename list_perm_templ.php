7ac775c6978b0845ac651e62925272c68efdc8f8
Edmondas Girkantas <edmondas@girkantas.lt>
2022-03-11 20:25:36 +0200

    Permission template output moved to external file.

--- list_perm_templ.php
+++ list_perm_templ.php
@@ -28,47 +28,22 @@
  * @copyright   2010-2022  Poweradmin Development Team
  * @license     https://opensource.org/licenses/GPL-3.0 GPL
  */
+
+use Poweradmin\AppFactory;
+
 require_once 'inc/toolkit.inc.php';
 include_once 'inc/header.inc.php';
 
-do_hook('verify_permission', 'templ_perm_edit') ? $perm_templ_perm_edit = "1" : $perm_templ_perm_edit = "0";
+$perm_templ_perm_edit = do_hook('verify_permission', 'templ_perm_edit');
 
-$permission_templates = do_hook('list_permission_templates');
-
-if ($perm_templ_perm_edit == "0") {
+if (!$perm_templ_perm_edit) {
     error(ERR_PERM_EDIT_PERM_TEMPL);
 } else {
-    echo "    <h2>" . _('Permission templates') . "</h2>\n";
-    echo "     <table>\n";
-    echo "      <tr>\n";
-    echo "       <th>&nbsp;</th>\n";
-    echo "       <th>" . _('Name') . "</th>\n";
-    echo "       <th>" . _('Description') . "</th>\n";
-    echo "      </tr>\n";
-
-    foreach ($permission_templates as $template) {
-
-        $perm_item_list = do_hook('get_permissions_by_template_id', $template['id'], true);
-        $perm_items = implode(', ', $perm_item_list);
-
-        echo "      <tr>\n";
-        if ($perm_templ_perm_edit == "1") {
-            echo "       <td>\n";
-            echo "        <a href=\"edit_perm_templ.php?id=" . $template["id"] . "\"><img src=\"images/edit.gif\" alt=\"[ " . _('Edit template') . " ]\"></a>\n";
-            echo "        <a href=\"delete_perm_templ.php?id=" . $template["id"] . "\"><img src=\"images/delete.gif\" alt=\"[ " . _('Delete template') . " ]\"></a>\n";
-            echo "       </td>\n";
-        } else {
-            echo "       <td>&nbsp;</td>\n";
-        }
-        echo "       <td class=\"y\">" . $template['name'] . "</td>\n";
-        echo "       <td class=\"y\">" . $template['descr'] . "</td>\n";
-        echo "      </tr>\n";
-    }
-
-    echo "     </table>\n";
-    echo "     <ul>\n";
-    echo "      <li><a href=\"add_perm_templ.php\">" . _('Add permission template') . "</a>.</li>\n";
-    echo "     </ul>\n";
+    $app = AppFactory::create();
+    $app->render('list_perm_templ.html', [
+        'perm_templ_perm_edit' => $perm_templ_perm_edit,
+        'permission_templates' => do_hook('list_permission_templates')
+    ]);
 }
 
 include_once("inc/footer.inc.php");
