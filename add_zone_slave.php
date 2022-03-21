9e78409d7f3c107a5f5c482219f777899eb33b07
Edmondas Girkantas <edmondas@girkantas.lt>
2022-03-21 21:32:16 +0200

    Code simplified.

--- add_zone_slave.php
+++ add_zone_slave.php
@@ -58,13 +58,10 @@ if (isset($_POST['slave_master'])) {
 
 $type = "SLAVE";
 
-/*
-  Check permissions
- */
-(do_hook('verify_permission' , 'zone_slave_add' )) ? $zone_slave_add = "1" : $zone_slave_add = "0";
-(do_hook('verify_permission' , 'user_view_others' )) ? $perm_view_others = "1" : $perm_view_others = "0";
+$zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
+$perm_view_others = do_hook('verify_permission', 'user_view_others');
 
-if (isset($_POST['submit']) && $zone_slave_add == "1") {
+if (isset($_POST['submit']) && $zone_slave_add) {
     if (!Dns::is_valid_hostname_fqdn($zone, 0)) {
         error(ERR_DNS_HOSTNAME);
     } elseif ($dns_third_level_check && DnsRecord::get_domain_level($zone) > 2 && DnsRecord::domain_exists(DnsRecord::get_second_level_domain($zone))) {
@@ -77,60 +74,62 @@ if (isset($_POST['submit']) && $zone_slave_add == "1") {
         if (DnsRecord::add_domain($zone, $owner, $type, $master, 'none')) {
             success("<a href=\"edit.php?id=" . DnsRecord::get_zone_id_from_name($zone) . "\">" . SUC_ZONE_ADD . '</a>');
             Syslog::log_info(sprintf('client_ip:%s user:%s operation:add_zone zone:%s zone_type:SLAVE zone_master:%s',
-                              $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
-                              $zone, $master));
+                $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
+                $zone, $master));
             unset($zone, $owner, $webip, $mailip, $empty, $type, $master);
         }
     }
 }
 
-if ($zone_slave_add != "1") {
+if (!$zone_slave_add) {
     error(ERR_PERM_ADD_ZONE_SLAVE);
-} else {
-    echo "     <h2>" . _('Add slave zone') . "</h2>\n";
-
-    $users = do_hook('show_users');
-    echo "     <form method=\"post\" action=\"add_zone_slave.php\">\n";
-    echo "      <table>\n";
-    echo "       <tr>\n";
-    echo "        <td class=\"n\" width=\"200\">" . _('Zone name') . "</td>\n";
-    echo "        <td class=\"n\">\n";
-    echo "         <input type=\"text\" class=\"input\" name=\"domain\" value=\"\">\n";
-    echo "        </td>\n";
-    echo "       </tr>\n";
-    echo "       <tr>\n";
-    echo "        <td class=\"n\">" . _('IP address of master NS') . ":</td>\n";
-    echo "        <td class=\"n\">\n";
-    echo "         <input type=\"text\" class=\"input\" name=\"slave_master\" value=\"\">\n";
-    echo "        </td>\n";
-    echo "       </tr>\n";
-    echo "       <tr>\n";
-    echo "        <td class=\"n\">" . _('Owner') . ":</td>\n";
-    echo "        <td class=\"n\">\n";
-    echo "         <select name=\"owner\">\n";
-    /*
-      Display list of users to assign slave zone to if the
-      editing user has the permissions to, otherwise just
-      display the adding users name
-     */
-    foreach ($users as $user) {
-        if ($user['id'] === $_SESSION['userid']) {
-            echo "          <option value=\"" . $user['id'] . "\" selected>" . $user['fullname'] . "</option>\n";
-        } elseif ($perm_view_others == "1") {
-            echo "          <option value=\"" . $user['id'] . "\">" . $user['fullname'] . "</option>\n";
-        }
-    }
-    echo "         </select>\n";
-    echo "        </td>\n";
-    echo "       </tr>\n";
-    echo "       <tr>\n";
-    echo "        <td class=\"n\">&nbsp;</td>\n";
-    echo "        <td class=\"n\">\n";
-    echo "         <input type=\"submit\" class=\"button\" name=\"submit\" value=\"" . _('Add zone') . "\">\n";
-    echo "        </td>\n";
-    echo "       </tr>\n";
-    echo "      </table>\n";
-    echo "     </form>\n";
+    include_once('inc/footer.inc.php');
+    die();
 }
 
-include_once("inc/footer.inc.php");
+echo "     <h2>" . _('Add slave zone') . "</h2>\n";
+
+$users = do_hook('show_users');
+echo "     <form method=\"post\" action=\"add_zone_slave.php\">\n";
+echo "      <table>\n";
+echo "       <tr>\n";
+echo "        <td class=\"n\" width=\"200\">" . _('Zone name') . "</td>\n";
+echo "        <td class=\"n\">\n";
+echo "         <input type=\"text\" class=\"input\" name=\"domain\" value=\"\">\n";
+echo "        </td>\n";
+echo "       </tr>\n";
+echo "       <tr>\n";
+echo "        <td class=\"n\">" . _('IP address of master NS') . ":</td>\n";
+echo "        <td class=\"n\">\n";
+echo "         <input type=\"text\" class=\"input\" name=\"slave_master\" value=\"\">\n";
+echo "        </td>\n";
+echo "       </tr>\n";
+echo "       <tr>\n";
+echo "        <td class=\"n\">" . _('Owner') . ":</td>\n";
+echo "        <td class=\"n\">\n";
+echo "         <select name=\"owner\">\n";
+/*
+  Display list of users to assign slave zone to if the
+  editing user has the permissions to, otherwise just
+  display the adding users name
+ */
+foreach ($users as $user) {
+    if ($user['id'] === $_SESSION['userid']) {
+        echo "          <option value=\"" . $user['id'] . "\" selected>" . $user['fullname'] . "</option>\n";
+    } elseif ($perm_view_others) {
+        echo "          <option value=\"" . $user['id'] . "\">" . $user['fullname'] . "</option>\n";
+    }
+}
+echo "         </select>\n";
+echo "        </td>\n";
+echo "       </tr>\n";
+echo "       <tr>\n";
+echo "        <td class=\"n\">&nbsp;</td>\n";
+echo "        <td class=\"n\">\n";
+echo "         <input type=\"submit\" class=\"button\" name=\"submit\" value=\"" . _('Add zone') . "\">\n";
+echo "        </td>\n";
+echo "       </tr>\n";
+echo "      </table>\n";
+echo "     </form>\n";
+
+include_once('inc/footer.inc.php');
