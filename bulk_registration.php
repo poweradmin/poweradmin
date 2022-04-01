c6ae3d8ce0dd1506daa9b791e9f175e25c7f523b
Edmondas Girkantas <edmondas@girkantas.lt>
2022-04-01 20:50:29 +0300

    Use templates for bulk import.

--- bulk_registration.php
+++ bulk_registration.php
@@ -42,7 +42,6 @@ require_once 'inc/message.inc.php';
 include_once 'inc/header.inc.php';
 
 $app = AppFactory::create();
-$iface_zone_type_default = $app->config('iface_zone_type_default');
 
 $owner = "-1";
 if ((isset($_POST['owner'])) && (Validation::is_number($_POST['owner']))) {
@@ -54,6 +53,7 @@ if (isset($_POST["dom_type"]) && (in_array($_POST['dom_type'], ZoneType::getType
     $dom_type = $_POST["dom_type"];
 }
 
+$domains = array();
 if (isset($_POST['domains'])) {
     $domains = explode("\r\n", $_POST['domains']);
     foreach ($domains as $key => $domain) {
@@ -64,8 +64,6 @@ if (isset($_POST['domains'])) {
             $domains[$key] = $domain;
         }
     }
-} else {
-    $domains = array();
 }
 
 $zone_template = $_POST['zone_template'] ?? "none";
@@ -73,8 +71,9 @@ $zone_template = $_POST['zone_template'] ?? "none";
 $zone_master_add = do_hook('verify_permission', 'zone_master_add');
 $perm_view_others = do_hook('verify_permission', 'user_view_others');
 
+$error = false;
+
 if (isset($_POST['submit']) && $zone_master_add) {
-    $error = false;
     foreach ($domains as $domain) {
         if (!Dns::is_valid_hostname_fqdn($domain, 0)) {
             error($domain . ' failed - ' . ERR_DNS_HOSTNAME);
@@ -86,8 +85,8 @@ if (isset($_POST['submit']) && $zone_master_add) {
         }
     }
 
-    if (false === $error) {
-        unset($domains, $owner, $dom_type, $zone_template);
+    if ($error === false) {
+        unset($owner, $dom_type, $zone_template);
     }
 }
 
@@ -96,76 +95,16 @@ if (!$zone_master_add) {
     include_once('inc/footer.inc.php');
     exit;
 }
-echo "     <h2>" . _('Bulk registration') . "</h2>\n";
 
-$available_zone_types = array("MASTER", "NATIVE");
-$users = do_hook('show_users');
-$zone_templates = ZoneTemplate::get_list_zone_templ($_SESSION['userid']);
-
-echo "     <form method=\"post\" action=\"bulk_registration.php\">\n";
-echo "      <table>\n";
-echo "       <tr>\n";
-echo "        <td class=\"n\" width=\"100\">" . _('Owner') . ":</td>\n";
-echo "        <td class=\"n\">\n";
-echo "         <select name=\"owner\">\n";
-/*
-  Display list of users to assign zone to if creating
-  user has the proper permission to do so.
- */
-foreach ($users as $user) {
-    if ($user['id'] === $_SESSION['userid']) {
-        echo "          <option value=\"" . $user['id'] . "\" selected>" . $user['fullname'] . "</option>\n";
-    } elseif ($perm_view_others) {
-        echo "          <option value=\"" . $user['id'] . "\">" . $user['fullname'] . "</option>\n";
-    }
-}
-echo "         </select>\n";
-echo "        </td>\n";
-echo "       </tr>\n";
-echo "       <tr>\n";
-echo "        <td class=\"n\">" . _('Type') . ":</td>\n";
-echo "        <td class=\"n\">\n";
-echo "         <select name=\"dom_type\">\n";
-foreach ($available_zone_types as $type) {
-    $type == $iface_zone_type_default ? $selected = ' selected' : $selected = '';
-    echo "          <option value=\"" . $type . "\" $selected>" . strtolower($type) . "</option>\n";
-}
-echo "         </select>\n";
-echo "        </td>\n";
-echo "       </tr>\n";
-echo "       <tr>\n";
-echo "        <td class=\"n\">" . _('Template') . ":</td>\n";
-echo "        <td class=\"n\">\n";
-echo "         <select name=\"zone_template\">\n";
-echo "          <option value=\"none\">none</option>\n";
-foreach ($zone_templates as $zone_template) {
-    echo "          <option value=\"" . $zone_template['id'] . "\">" . $zone_template['name'] . "</option>\n";
-}
-echo "         </select>\n";
-echo "        </td>\n";
-echo "       </tr>\n";
-
-echo "       <tr>\n";
-echo "        <td class=\"n\">" . _('Zones') . ":</td>\n";
-echo "        <td class=\"n\">\n";
-echo "         <ul id=\"domain_names\" style=\"list-style-type:none; padding:0 \">\n";
-echo "		<li>" . _('Type one domain per line') . ":</li>\n";
-echo "          <li><textarea class=\"input\" name=\"domains\" rows=\"10\" cols=\"30\" style=\"width: 500px;\">";
-if (isset($error) && isset($_POST['domains'])) {
-    echo $_POST['domains'];
-}
-echo "</textarea></li>\n";
-echo "         </ol>\n";
-echo "        </td>\n";
-echo "       </tr>\n";
-
-echo "       <tr>\n";
-echo "        <td class=\"n\">&nbsp;</td>\n";
-echo "        <td class=\"n\">\n";
-echo "         <input type=\"submit\" class=\"button\" name=\"submit\" value=\"" . _('Add zones') . "\">\n";
-echo "        </td>\n";
-echo "       </tr>\n";
-echo "      </table>\n";
-echo "     </form>\n";
+$app->render('bulk_registration.html', [
+    'userid' => $_SESSION['userid'],
+    'error' => $error,
+    'domains' => $domains,
+    'perm_view_others' => $perm_view_others,
+    'iface_zone_type_default' => $app->config('iface_zone_type_default'),
+    'available_zone_types' => array("MASTER", "NATIVE"),
+    'users' => do_hook('show_users'),
+    'zone_templates' => ZoneTemplate::get_list_zone_templ($_SESSION['userid']),
+]);
 
 include_once('inc/footer.inc.php');
