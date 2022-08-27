734716fc361a133e6b06c723ebedee2be513364f
Edmondas Girkantas <edmondas@girkantas.lt>
2022-08-27 15:25:32 +0300

    Navigation menu moved to template.

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -37,67 +37,31 @@ global $session_key;
 
 header('Content-type: text/html; charset=utf-8');
 
-$app = AppFactory::create();
-$app->render('header.html', [
+$vars = [
     'iface_title' => $iface_title,
     'iface_style' => $iface_style,
     'file_version' => time(),
     'custom_header' => file_exists('templates/custom/header.html'),
-]);
-
-if ($ignore_install_dir == false && file_exists('install')) {
-    echo "<div>\n";
-    error(ERR_INSTALL_DIR_EXISTS);
-    include('inc/footer.inc.php');
-    exit();
-}
+    'install_error' => !$ignore_install_dir && file_exists('install') ? ERR_INSTALL_DIR_EXISTS : false,
+];
 
 if (isset($_SESSION ["userid"])) {
-    $perm_search = do_hook('verify_permission', 'search');
-    $perm_view_zone_own = do_hook('verify_permission', 'zone_content_view_own');
-    $perm_view_zone_other = do_hook('verify_permission', 'zone_content_view_others');
-    $perm_supermaster_view = do_hook('verify_permission', 'supermaster_view');
-    $perm_zone_master_add = do_hook('verify_permission', 'zone_master_add');
-    $perm_zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
-    $perm_supermaster_add = do_hook('verify_permission', 'supermaster_add');
     $perm_is_godlike = do_hook('verify_permission', 'user_is_ueberuser');
 
-    if ($perm_is_godlike && $session_key == 'p0w3r4dm1n') {
-        error(ERR_DEFAULT_CRYPTOKEY_USED);
-        echo "<br>";
-    }
-
-    echo "    <div class=\"menu\">\n";
-    echo "    <span class=\"menuitem\"><a href=\"index.php\">" . _('Index') . "</a></span>\n";
-    if ($perm_search) {
-        echo "    <span class=\"menuitem\"><a href=\"search.php\">" . _('Search zones and records') . "</a></span>\n";
-    }
-    if ($perm_view_zone_own || $perm_view_zone_other) {
-        echo "    <span class=\"menuitem\"><a href=\"list_zones.php\">" . _('List zones') . "</a></span>\n";
-    }
-    if ($perm_zone_master_add) {
-        echo "    <span class=\"menuitem\"><a href=\"list_zone_templ.php\">" . _('List zone templates') . "</a></span>\n";
-    }
-    if ($perm_supermaster_view) {
-        echo "    <span class=\"menuitem\"><a href=\"list_supermasters.php\">" . _('List supermasters') . "</a></span>\n";
-    }
-    if ($perm_zone_master_add) {
-        echo "    <span class=\"menuitem\"><a href=\"add_zone_master.php\">" . _('Add master zone') . "</a></span>\n";
-    }
-    if ($perm_zone_slave_add) {
-        echo "    <span class=\"menuitem\"><a href=\"add_zone_slave.php\">" . _('Add slave zone') . "</a></span>\n";
-    }
-    if ($perm_supermaster_add) {
-        echo "    <span class=\"menuitem\"><a href=\"add_supermaster.php\">" . _('Add supermaster') . "</a></span>\n";
-    }
-    if ($perm_zone_master_add) {
-        echo "    <span class=\"menuitem\"><a href=\"bulk_registration.php\">" . _('Bulk registration') . "</a></span>\n";
-    }
-    if ($_SESSION ["auth_used"] != "ldap") {
-        echo "    <span class=\"menuitem\"><a href=\"change_password.php\">" . _('Change password') . "</a></span>\n";
-    }
-    echo "    <span class=\"menuitem\"><a href=\"users.php\">" . _('User administration') . "</a></span>\n";
-    echo "    <span class=\"menuitem\"><a href=\"index.php?logout\">" . _('Logout') . "</a></span>\n";
-    echo "    </div> <!-- /menu -->\n";
+    $vars = array_merge($vars, [
+        'user_logged_in' => isset($_SESSION ["userid"]),
+        'perm_search' => do_hook('verify_permission', 'search'),
+        'perm_view_zone_own' => do_hook('verify_permission', 'zone_content_view_own'),
+        'perm_view_zone_other' => do_hook('verify_permission', 'zone_content_view_others'),
+        'perm_supermaster_view' => do_hook('verify_permission', 'supermaster_view'),
+        'perm_zone_master_add' => do_hook('verify_permission', 'zone_master_add'),
+        'perm_zone_slave_add' => do_hook('verify_permission', 'zone_slave_add'),
+        'perm_supermaster_add' => do_hook('verify_permission', 'supermaster_add'),
+        'perm_is_godlike' => $perm_is_godlike,
+        'session_key_error' => $perm_is_godlike && $session_key == 'p0w3r4dm1n' ? ERR_DEFAULT_CRYPTOKEY_USED : false,
+        'auth_used' => $_SESSION["auth_used"] != "ldap"
+    ]);
 }
-echo "    <div class=\"content\">\n";
+
+$app = AppFactory::create();
+$app->render('header.html', $vars);
