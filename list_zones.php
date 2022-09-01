c12ca0a93c398768bee0085d6fbfd01d153a508f
Edmondas Girkantas <edmondas@girkantas.lt>
2022-09-01 23:00:59 +0300

    Disable checkbox if domain is not secured.

--- list_zones.php
+++ list_zones.php
@@ -171,7 +171,7 @@ if ($perm_view == "none") {
             }
         }
         if ($pdnssec_use) {
-            echo "          <td><input type=\"checkbox\" onclick=\"return false\" " . ($zone["secured"] ? 'checked' : '') . "></td>\n";
+            echo "          <td><input type=\"checkbox\" disabled " . ($zone["secured"] ? 'checked' : '') . "></td>\n";
         }
         echo "          <td>\n";
         echo "           <a class=\"btn btn-outline-primary btn-sm\" href=\"edit.php?name=" . $zone['name'] . "&id=" . $zone['id'] . "\"><i class=\"bi bi-pencil-square\"></i> " . _('Edit zone') . "</a>\n";
