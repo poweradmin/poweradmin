f1cb0a1171fc54c914ad21f1fd1c5241e085b370
Edmondas Girkantas <edmondas@girkantas.lt>
2022-09-04 16:27:38 +0300

    Reformated the code, added use of defined variable.

--- addons/dynamic_dns_client.pl
+++ addons/dynamic_dns_client.pl
@@ -6,22 +6,20 @@ use strict;
 use warnings;
 
 # change these values
-my $login          = 'username';
-my $password       = 'password';
-my $domain         = 'mydynamicdns.example.com';
+my $login = 'username';
+my $password = 'password';
+my $domain = 'mydynamicdns.example.com';
 my $poweradmin_url = 'http://www.example.com/poweradmin';
-my $up_update_url  = $poweradmin_url . '/dynamic_update.php';
-my $ip_lookup_url  = $poweradmin_url . '/addons/clientip.php';
-my $verbose        = 1;
+my $ip_update_url = $poweradmin_url . '/dynamic_update.php';
+my $ip_lookup_url = $poweradmin_url . '/addons/clientip.php';
+my $verbose = 1;
 
 my $ip_address = LWP::Simple::get($ip_lookup_url)
-  or die("Error: Could not get your global IP address!\n");
+    or die("Error: Could not get your global IP address!\n");
 
 # FIXME: doesn't support IPv6
-if ( $ip_address !~/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/ )
-{
-    print
-      "Error: Invalid global IP address! Check if Poweradmin url is correct\n";
+if ($ip_address !~ /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/) {
+    print "Error: Invalid global IP address! Check if Poweradmin url is correct\n";
     exit;
 }
 
@@ -30,12 +28,10 @@ print "Updating the IP address ($ip_address) now ... \n" if $verbose;
 # insert authentication data to url
 $poweradmin_url =~ s/^(http[s]?:\/\/)/$1$login:$password\@/;
 
-my $response =
-  LWP::Simple::get( "$poweradmin_url/dynamic_update.php"
-      . "?hostname=$domain&myip=$ip_address&verbose=$verbose" )
-  or die($!);
+my $response = LWP::Simple::get($ip_update_url . "?hostname=$domain&myip=$ip_address&verbose=$verbose")
+    or die($!);
 
-if ( !defined $response || $response eq "" ) {
+if (!defined $response || $response eq "") {
     print "Error: Could not contact your poweradmin web server\n";
     exit(0);
 }
