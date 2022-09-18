96d01f5c68d6da855c0a47083293ddfed5ee026d
Edmondas Girkantas <edmondas@girkantas.lt>
2022-09-18 20:22:53 +0300

    Reformatted the code.

--- lib/RecordLog.php
+++ lib/RecordLog.php
@@ -22,26 +22,31 @@
 
 namespace Poweradmin;
 
-class RecordLog {
+class RecordLog
+{
 
     private $record_prior;
     private $record_after;
 
     private $record_changed = false;
 
-    public function log_prior($rid) {
+    public function log_prior($rid)
+    {
         $this->record_prior = $this->getRecord($rid);
     }
 
-    public function log_after($rid) {
+    public function log_after($rid)
+    {
         $this->record_after = $this->getRecord($rid);
     }
 
-    private function getRecord($rid) {
+    private function getRecord($rid)
+    {
         return DnsRecord::get_record_from_id($rid);
     }
 
-    public function has_changed(array $record) {
+    public function has_changed(array $record)
+    {
         // Arrays are assigned by copy.
         // Copy arrays to avoid side effects caused by unset().
         $record_copy = $record;
@@ -62,7 +67,8 @@ class RecordLog {
         return $this->record_changed;
     }
 
-    public function write() {
+    public function write()
+    {
         Logger::log_info(sprintf('client_ip:%s user:%s operation:edit_record'
             . ' old_record_type:%s old_record:%s old_content:%s old_ttl:%s old_priority:%s'
             . ' record_type:%s record:%s content:%s ttl:%s priority:%s',
