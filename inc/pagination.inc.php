527f205ddcf5a17275f86b30a814d3d29bcabf29
Edmondas Girkantas <edmondas@girkantas.lt>
2022-11-27 19:21:00 +0200

    Handle case when iface_rowamount is zero, related to #474 issue.

--- inc/pagination.inc.php
+++ inc/pagination.inc.php
@@ -29,7 +29,7 @@
  * @return string $result
  */
 function show_pages($amount, $rowamount, $id = '') {
-    if ($amount <= $rowamount) {
+    if ($amount <= $rowamount || $rowamount == 0) {
         return "";
     }
 
