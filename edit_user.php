29de35ed15a8cda0ab11bf3471951b7c859b47b7
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-17 20:44:29 +0200

    Minimize code nesting.

--- edit_user.php
+++ edit_user.php
@@ -41,143 +41,148 @@ if (isset($_GET['id']) && Validation::is_number($_GET['id'])) {
     $edit_id = $_GET['id'];
 }
 
-do_hook('verify_permission' , 'user_edit_own' ) ? $perm_edit_own = "1" : $perm_edit_own = "0";
-do_hook('verify_permission' , 'user_edit_others' ) ? $perm_edit_others = "1" : $perm_edit_others = "0";
+do_hook('verify_permission', 'user_edit_own') ? $perm_edit_own = "1" : $perm_edit_own = "0";
+do_hook('verify_permission', 'user_edit_others') ? $perm_edit_others = "1" : $perm_edit_others = "0";
 
 if ($edit_id == "-1") {
     error(ERR_INV_INPUT);
-} elseif (($edit_id == $_SESSION["userid"] && $perm_edit_own == "1") || ($edit_id != $_SESSION["userid"] && $perm_edit_others == "1" )) {
+    include_once("inc/footer.inc.php");
+    exit;
+}
 
-    if (isset($_POST["commit"])) {
-
-        $i_username = "-1";
-        $i_fullname = "-1";
-        $i_email = "-1";
-        $i_description = "-1";
-        $i_password = "-1";
-        $i_perm_templ = "0";
-        $i_active = "0";
-
-        if (isset($_POST['username'])) {
-            $i_username = $_POST['username'];
-        }
-
-        if (isset($_POST['fullname'])) {
-            $i_fullname = $_POST['fullname'];
-        }
-
-        if (isset($_POST['email'])) {
-            $i_email = $_POST['email'];
-        }
-
-        if (isset($_POST['description'])) {
-            $i_description = $_POST['description'];
-        }
-
-        if (isset($_POST['password'])) {
-            $i_password = $_POST['password'];
-        }
-
-        if (isset($_POST['perm_templ']) && Validation::is_number($_POST['perm_templ'])) {
-            $i_perm_templ = $_POST['perm_templ'];
-        }
-
-        if (isset($_POST['active']) && Validation::is_number($_POST['active'])) {
-            $i_active = $_POST['active'];
-        }
-
-        if ($i_username == "-1" || $i_fullname == "-1" || $i_email < "1" || $i_description == "-1" || $i_password == "-1") {
-            error(ERR_INV_INPUT);
-        } else {
-            if ($i_username != "" && $i_perm_templ > "0" && $i_fullname) {
-                if (!isset($i_active)) {
-                    $active = 0;
-                } else {
-                    $active = 1;
-                }
-                if (do_hook('edit_user' , $edit_id, $i_username, $i_fullname, $i_email, $i_perm_templ, $i_description, $active, $i_password )) {
-                    success(SUC_USER_UPD);
-                }
-            }
-        }
-    }
-
-    $users = do_hook('get_user_detail_list' , $edit_id );
-
-    foreach ($users as $user) {
-
-        (($user['active']) == "1") ? $check = " CHECKED" : $check = "";
-        $name = $user['fullname'] ?: $user['username'];
-
-        echo "     <h5 class=\"mb-3\">" . _('Edit user') . " \"" . $name . "\"</h5>\n";
-        echo "     <form class=\"needs-validation\" method=\"post\" action=\"\" novalidate>\n";
-        echo "      <input type=\"hidden\" name=\"number\" value=\"" . htmlspecialchars($edit_id) . "\">\n";
-        echo "      <table>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Username') . "</td>\n";
-        echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"username\" value=\"" . htmlspecialchars($user['username']) . "\" required>\n";
-        echo "        <div class=\"invalid-feedback\">" . _('Provide user name') . "</div>";
-        echo "        </td>\n";
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Fullname') . "</td>\n";
-        echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"fullname\" value=\"" . htmlspecialchars($user['fullname']) . "\" required></td>\n";
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Password') . "</td>\n";
-        echo "        <td><input class=\"form-control form-control-sm\" type=\"password\" name=\"password\"></td>\n";
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Email address') . "</td>\n";
-        echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"email\" value=\"" . htmlspecialchars($user['email']) . "\" required>\n";
-        echo "        <div class=\"invalid-feedback\">" . _('Provide email') . "</div>";
-        echo "        </td>\n";
-        echo "       </tr>\n";
-        if (do_hook('verify_permission' , 'user_edit_templ_perm' )) {
-            echo "       <tr>\n";
-            echo "        <td>" . _('Permission template') . "</td>\n";
-            echo "        <td>\n";
-            echo "         <select class=\"form-select form-select-sm\" name=\"perm_templ\">\n";
-            foreach (do_hook('list_permission_templates' ) as $template) {
-                ($template['id'] == $user['tpl_id']) ? $select = " SELECTED" : $select = "";
-                echo "          <option value=\"" . $template['id'] . "\"" . $select . ">" . $template['name'] . "</option>\n";
-            }
-            echo "         </select>\n";
-            echo "       </td>\n";
-        }
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Description') . "</td>\n";
-        echo "        <td><textarea class=\"form-control form-control-sm\" rows=\"4\" cols=\"30\" class=\"inputarea\" name=\"description\">" . htmlspecialchars($user['descr']) . "</textarea></td>\n";
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>" . _('Enabled') . "</td>\n";
-        echo "        <td><input class=\"form-check-input\" type=\"checkbox\" name=\"active\" value=\"1\"" . $check . "></td>\n";
-        echo "       </tr>\n";
-        echo "       <tr>\n";
-        echo "        <td>&nbsp;</td>\n";
-        echo "        <td><input class=\"btn btn-primary btn-sm\" type=\"submit\" name=\"commit\" value=\"" . _('Commit changes') . "\">\n";
-        echo "        <input class=\"btn btn-secondary btn-sm\" type=\"reset\" name=\"reset\" value=\"" . _('Reset changes') . "\"></td>\n";
-        echo "      </table>\n";
-        echo "     </form>\n";
-
-        echo "     <p>\n";
-        echo "<div class=\"pt-3 text-secondary\">";
-        printf(_('This user has been assigned the permission template "%s".'), $user['tpl_name']);
-        if ($user['tpl_descr'] != "") {
-            echo " " . _('The description for this template is') . ": \"" . htmlspecialchars($user['tpl_descr']) . "\".";
-        }
-        echo " " . _('Based on this template, this user has the following permissions') . ":";
-        echo "     </p>\n";
-        echo "     <ul>\n";
-        foreach (do_hook('get_permissions_by_template_id' , $user['tpl_id'] ) as $item) {
-            echo "      <li>" . _(htmlspecialchars($item['descr'])) . " (" . htmlspecialchars($item['name']) . ")</li>\n";
-        }
-        echo "     </ul>\n";
-        echo "</div>";
-    }
-} else {
+if (($edit_id != $_SESSION["userid"] || $perm_edit_own != "1") && ($edit_id == $_SESSION["userid"] || $perm_edit_others != "1")) {
     error(ERR_PERM_EDIT_USER);
+    include_once("inc/footer.inc.php");
+    exit;
+}
+
+if (isset($_POST["commit"])) {
+
+    $i_username = "-1";
+    $i_fullname = "-1";
+    $i_email = "-1";
+    $i_description = "-1";
+    $i_password = "-1";
+    $i_perm_templ = "0";
+    $i_active = "0";
+
+    if (isset($_POST['username'])) {
+        $i_username = $_POST['username'];
+    }
+
+    if (isset($_POST['fullname'])) {
+        $i_fullname = $_POST['fullname'];
+    }
+
+    if (isset($_POST['email'])) {
+        $i_email = $_POST['email'];
+    }
+
+    if (isset($_POST['description'])) {
+        $i_description = $_POST['description'];
+    }
+
+    if (isset($_POST['password'])) {
+        $i_password = $_POST['password'];
+    }
+
+    if (isset($_POST['perm_templ']) && Validation::is_number($_POST['perm_templ'])) {
+        $i_perm_templ = $_POST['perm_templ'];
+    }
+
+    if (isset($_POST['active']) && Validation::is_number($_POST['active'])) {
+        $i_active = $_POST['active'];
+    }
+
+    if ($i_username == "-1" || $i_fullname == "-1" || $i_email < "1" || $i_description == "-1" || $i_password == "-1") {
+        error(ERR_INV_INPUT);
+    } else {
+        if ($i_username != "" && $i_perm_templ > "0" && $i_fullname) {
+            if (!isset($i_active)) {
+                $active = 0;
+            } else {
+                $active = 1;
+            }
+            if (do_hook('edit_user', $edit_id, $i_username, $i_fullname, $i_email, $i_perm_templ, $i_description, $active, $i_password)) {
+                success(SUC_USER_UPD);
+            }
+        }
+    }
+}
+
+$users = do_hook('get_user_detail_list', $edit_id);
+
+foreach ($users as $user) {
+
+    (($user['active']) == "1") ? $check = " CHECKED" : $check = "";
+    $name = $user['fullname'] ?: $user['username'];
+
+    echo "     <h5 class=\"mb-3\">" . _('Edit user') . " \"" . $name . "\"</h5>\n";
+    echo "     <form class=\"needs-validation\" method=\"post\" action=\"\" novalidate>\n";
+    echo "      <input type=\"hidden\" name=\"number\" value=\"" . htmlspecialchars($edit_id) . "\">\n";
+    echo "      <table>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Username') . "</td>\n";
+    echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"username\" value=\"" . htmlspecialchars($user['username']) . "\" required>\n";
+    echo "        <div class=\"invalid-feedback\">" . _('Provide user name') . "</div>";
+    echo "        </td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Fullname') . "</td>\n";
+    echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"fullname\" value=\"" . htmlspecialchars($user['fullname']) . "\" required></td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Password') . "</td>\n";
+    echo "        <td><input class=\"form-control form-control-sm\" type=\"password\" name=\"password\"></td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Email address') . "</td>\n";
+    echo "        <td><input class=\"form-control form-control-sm\" type=\"text\" name=\"email\" value=\"" . htmlspecialchars($user['email']) . "\" required>\n";
+    echo "        <div class=\"invalid-feedback\">" . _('Provide email') . "</div>";
+    echo "        </td>\n";
+    echo "       </tr>\n";
+    if (do_hook('verify_permission', 'user_edit_templ_perm')) {
+        echo "       <tr>\n";
+        echo "        <td>" . _('Permission template') . "</td>\n";
+        echo "        <td>\n";
+        echo "         <select class=\"form-select form-select-sm\" name=\"perm_templ\">\n";
+        foreach (do_hook('list_permission_templates') as $template) {
+            ($template['id'] == $user['tpl_id']) ? $select = " SELECTED" : $select = "";
+            echo "          <option value=\"" . $template['id'] . "\"" . $select . ">" . $template['name'] . "</option>\n";
+        }
+        echo "         </select>\n";
+        echo "       </td>\n";
+    }
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Description') . "</td>\n";
+    echo "        <td><textarea class=\"form-control form-control-sm\" rows=\"4\" cols=\"30\" class=\"inputarea\" name=\"description\">" . htmlspecialchars($user['descr']) . "</textarea></td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>" . _('Enabled') . "</td>\n";
+    echo "        <td><input class=\"form-check-input\" type=\"checkbox\" name=\"active\" value=\"1\"" . $check . "></td>\n";
+    echo "       </tr>\n";
+    echo "       <tr>\n";
+    echo "        <td>&nbsp;</td>\n";
+    echo "        <td><input class=\"btn btn-primary btn-sm\" type=\"submit\" name=\"commit\" value=\"" . _('Commit changes') . "\">\n";
+    echo "        <input class=\"btn btn-secondary btn-sm\" type=\"reset\" name=\"reset\" value=\"" . _('Reset changes') . "\"></td>\n";
+    echo "      </table>\n";
+    echo "     </form>\n";
+
+    echo "     <p>\n";
+    echo "<div class=\"pt-3 text-secondary\">";
+    printf(_('This user has been assigned the permission template "%s".'), $user['tpl_name']);
+    if ($user['tpl_descr'] != "") {
+        echo " " . _('The description for this template is') . ": \"" . htmlspecialchars($user['tpl_descr']) . "\".";
+    }
+    echo " " . _('Based on this template, this user has the following permissions') . ":";
+    echo "     </p>\n";
+    echo "     <ul>\n";
+    foreach (do_hook('get_permissions_by_template_id', $user['tpl_id']) as $item) {
+        echo "      <li>" . _(htmlspecialchars($item['descr'])) . " (" . htmlspecialchars($item['name']) . ")</li>\n";
+    }
+    echo "     </ul>\n";
+    echo "</div>";
 }
 
 include_once("inc/footer.inc.php");
