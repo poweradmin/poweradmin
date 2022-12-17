5e6154f302d41024b8c8d1bdfdbf600a87f11988
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-17 20:38:58 +0200

    Use MVC like pattern.

--- bulk_registration.php
+++ bulk_registration.php
@@ -29,89 +29,89 @@
  * @license     https://opensource.org/licenses/GPL-3.0 GPL
  */
 
-use Poweradmin\AppFactory;
+use Poweradmin\BaseController;
 use Poweradmin\Dns;
 use Poweradmin\DnsRecord;
 use Poweradmin\Logger;
-use Poweradmin\Validation;
 use Poweradmin\ZoneTemplate;
-use Poweradmin\ZoneType;
 
 require_once 'inc/toolkit.inc.php';
 require_once 'inc/message.inc.php';
 
-include_once 'inc/header.inc.php';
+class BulkRegistrationController extends BaseController {
 
-$app = AppFactory::create();
+    public function run(): void
+    {
+        $this->checkPermission('zone_master_add', ERR_PERM_ADD_ZONE_MASTER);
 
-$owner = "-1";
-if ((isset($_POST['owner'])) && (Validation::is_number($_POST['owner']))) {
-    $owner = $_POST['owner'];
-}
-
-$dom_type = "NATIVE";
-if (isset($_POST["dom_type"]) && (in_array($_POST['dom_type'], ZoneType::getTypes()))) {
-    $dom_type = $_POST["dom_type"];
-}
-
-$domains = array();
-if (isset($_POST['domains'])) {
-    $domains = explode("\r\n", $_POST['domains']);
-    foreach ($domains as $key => $domain) {
-        $domain = idn_to_ascii(trim($domain), IDNA_NONTRANSITIONAL_TO_ASCII);
-        if ($domain == '') {
-            unset($domains[$key]);
+        if ($this->isPost()) {
+            $this->doBulkRegistration();
         } else {
-            $domains[$key] = $domain;
-        }
-    }
-}
-
-$zone_template = $_POST['zone_template'] ?? "none";
-
-$zone_master_add = do_hook('verify_permission', 'zone_master_add');
-$perm_view_others = do_hook('verify_permission', 'user_view_others');
-
-$error = false;
-
-if (isset($_POST['submit']) && $zone_master_add) {
-    foreach ($domains as $domain) {
-        if (!Dns::is_valid_hostname_fqdn($domain, 0)) {
-            error($domain . ' failed - ' . ERR_DNS_HOSTNAME);
-        } elseif (DnsRecord::domain_exists($domain)) {
-            $idn_zone_name = idn_to_utf8($domain, IDNA_NONTRANSITIONAL_TO_ASCII);
-            error($idn_zone_name . " failed - " . ERR_DOMAIN_EXISTS);
-            $error = true;
-        } elseif (DnsRecord::add_domain($domain, $owner, $dom_type, '', $zone_template)) {
-            $zone_id = DnsRecord::get_zone_id_from_name($domain);
-            Logger::log_info(sprintf('client_ip:%s user:%s operation:add_zone zone:%s zone_type:%s zone_template:%s',
-                $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
-                $domain, $dom_type, $zone_template), $zone_id);
-            $idn_zone_name = idn_to_utf8($domain, IDNA_NONTRANSITIONAL_TO_ASCII);
-            success("<a href=\"edit.php?id=" . DnsRecord::get_zone_id_from_name($domain) . "\">" . $idn_zone_name . " - " . SUC_ZONE_ADD . '</a>');
+            $this->showBulkRegistrationForm();
         }
     }
 
-    if ($error === false) {
-        unset($owner, $dom_type, $zone_template);
+    private function doBulkRegistration()
+    {
+        $v = new Valitron\Validator($_POST);
+        $v->rules([
+            'required' => ['owner', 'dom_type', 'zone_template', 'domains'],
+            'integer' => ['owner'],
+        ]);
+
+        if ($v->validate()) {
+            $domains = $this->getDomains($_POST['domains']);
+            $dom_type = $_POST["dom_type"];
+            $zone_template = $_POST['zone_template'];
+
+            foreach ($domains as $domain) {
+                if (!Dns::is_valid_hostname_fqdn($domain, 0)) {
+                    error($domain . ' failed - ' . ERR_DNS_HOSTNAME);
+                } elseif (DnsRecord::domain_exists($domain)) {
+                    $idn_zone_name = idn_to_utf8($domain, IDNA_NONTRANSITIONAL_TO_ASCII);
+                    error($idn_zone_name . " failed - " . ERR_DOMAIN_EXISTS);
+                } elseif (DnsRecord::add_domain($domain, $_POST['owner'], $dom_type, '', $zone_template)) {
+                    $zone_id = DnsRecord::get_zone_id_from_name($domain);
+                    Logger::log_info(sprintf('client_ip:%s user:%s operation:add_zone zone:%s zone_type:%s zone_template:%s',
+                        $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
+                        $domain, $dom_type, $zone_template), $zone_id);
+                    $idn_zone_name = idn_to_utf8($domain, IDNA_NONTRANSITIONAL_TO_ASCII);
+                    success("<a href=\"edit.php?id=" . DnsRecord::get_zone_id_from_name($domain) . "\">" . $idn_zone_name . " - " . SUC_ZONE_ADD . '</a>');
+                }
+            }
+        } else {
+            $this->showError($v->errors());
+        }
+
+    }
+
+    private function showBulkRegistrationForm()
+    {
+        $this->render('bulk_registration.html', [
+            'userid' => $_SESSION['userid'],
+            'perm_view_others' => do_hook('verify_permission', 'user_view_others'),
+            'iface_zone_type_default' => $this->config('iface_zone_type_default'),
+            'available_zone_types' => array("MASTER", "NATIVE"),
+            'users' => do_hook('show_users'),
+            'zone_templates' => ZoneTemplate::get_list_zone_templ($_SESSION['userid']),
+        ]);
+    }
+
+    public function getDomains($array): array
+    {
+        $domains = explode("\r\n", $array);
+        foreach ($domains as $key => $domain) {
+            $domain = idn_to_ascii(trim($domain), IDNA_NONTRANSITIONAL_TO_ASCII);
+            if ($domain == '') {
+                unset($domains[$key]);
+            } else {
+                $domains[$key] = $domain;
+            }
+        }
+        return $domains;
     }
 }
 
-if (!$zone_master_add) {
-    error(ERR_PERM_ADD_ZONE_MASTER);
-    include_once('inc/footer.inc.php');
-    exit;
-}
+$controller = new BulkRegistrationController();
+$controller->run();
 
-$app->render('bulk_registration.html', [
-    'userid' => $_SESSION['userid'],
-    'error' => $error,
-    'domains' => $domains,
-    'perm_view_others' => $perm_view_others,
-    'iface_zone_type_default' => $app->config('iface_zone_type_default'),
-    'available_zone_types' => array("MASTER", "NATIVE"),
-    'users' => do_hook('show_users'),
-    'zone_templates' => ZoneTemplate::get_list_zone_templ($_SESSION['userid']),
-]);
-
-include_once('inc/footer.inc.php');
