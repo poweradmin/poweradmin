8eed47817736fda6736c0a169666844e5ca097bf
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-22 21:18:35 +0200

    Use MVC like controller.

--- edit.php
+++ edit.php
@@ -30,7 +30,7 @@
  * @license     https://opensource.org/licenses/GPL-3.0 GPL
  */
 
-use Poweradmin\AppFactory;
+use Poweradmin\BaseController;
 use Poweradmin\DnsRecord;
 use Poweradmin\Dnssec;
 use Poweradmin\Permission;
@@ -44,224 +44,228 @@ require_once 'inc/toolkit.inc.php';
 require_once 'inc/pagination.inc.php';
 require_once 'inc/message.inc.php';
 
-include_once 'inc/header.inc.php';
+class EditController extends BaseController {
 
-$app = AppFactory::create();
-$pdnssec_use = $app->config('pdnssec_use');
-$iface_add_reverse_record = $app->config('iface_add_reverse_record');
-$iface_rowamount = $app->config('iface_rowamount');
-$iface_zone_comments = $app->config('iface_zone_comments');
+    public function run(): void
+    {
+        $pdnssec_use = $this->config('pdnssec_use');
+        $iface_add_reverse_record = $this->config('iface_add_reverse_record');
+        $iface_rowamount = $this->config('iface_rowamount');
+        $iface_zone_comments = $this->config('iface_zone_comments');
 
-$row_start = 0;
-if (isset($_GET["start"])) {
-    $row_start = ($_GET["start"] - 1) * $iface_rowamount;
-}
+        $row_start = 0;
+        if (isset($_GET["start"])) {
+            $row_start = ($_GET["start"] - 1) * $iface_rowamount;
+        }
 
-$record_sort_by = 'name';
-if (isset($_GET["record_sort_by"]) && preg_match("/^[a-z_]+$/", $_GET["record_sort_by"])) {
-    $record_sort_by = $_GET["record_sort_by"];
-    $_SESSION["record_sort_by"] = $_GET["record_sort_by"];
-} elseif (isset($_POST["record_sort_by"]) && preg_match("/^[a-z_]+$/", $_POST["record_sort_by"])) {
-    $record_sort_by = $_POST["record_sort_by"];
-    $_SESSION["record_sort_by"] = $_POST["record_sort_by"];
-} elseif (isset($_SESSION["record_sort_by"])) {
-    $record_sort_by = $_SESSION["record_sort_by"];
-}
+        $record_sort_by = 'name';
+        if (isset($_GET["record_sort_by"]) && preg_match("/^[a-z_]+$/", $_GET["record_sort_by"])) {
+            $record_sort_by = $_GET["record_sort_by"];
+            $_SESSION["record_sort_by"] = $_GET["record_sort_by"];
+        } elseif (isset($_POST["record_sort_by"]) && preg_match("/^[a-z_]+$/", $_POST["record_sort_by"])) {
+            $record_sort_by = $_POST["record_sort_by"];
+            $_SESSION["record_sort_by"] = $_POST["record_sort_by"];
+        } elseif (isset($_SESSION["record_sort_by"])) {
+            $record_sort_by = $_SESSION["record_sort_by"];
+        }
 
-if (!isset($_GET['id']) || !Validation::is_number($_GET['id'])) {
-    error(ERR_INV_INPUT);
-    include_once("inc/footer.inc.php");
-    exit;
-}
-$zone_id = htmlspecialchars($_GET['id']);
+        if (!isset($_GET['id']) || !Validation::is_number($_GET['id'])) {
+            error(ERR_INV_INPUT);
+            include_once("inc/footer.inc.php");
+            exit;
+        }
+        $zone_id = htmlspecialchars($_GET['id']);
 
-if (isset($_POST['commit'])) {
-    $error = false;
-    $one_record_changed = false;
+        if (isset($_POST['commit'])) {
+            $error = false;
+            $one_record_changed = false;
 
-    if (isset($_POST['record'])) {
-        foreach ($_POST['record'] as $record) {
-            $old_record_info = DnsRecord::get_record_from_id($record['rid']);
+            if (isset($_POST['record'])) {
+                foreach ($_POST['record'] as $record) {
+                    $old_record_info = DnsRecord::get_record_from_id($record['rid']);
 
-            // Check if a record changed and save the state
-            $log = new RecordLog();
-            $log->log_prior($record['rid'], $record['zid']);
-            if (!$log->has_changed($record)) {
-                continue;
-            } else {
-                $one_record_changed = true;
+                    // Check if a record changed and save the state
+                    $log = new RecordLog();
+                    $log->log_prior($record['rid'], $record['zid']);
+                    if (!$log->has_changed($record)) {
+                        continue;
+                    } else {
+                        $one_record_changed = true;
+                    }
+
+                    $edit_record = DnsRecord::edit_record($record);
+                    if (false === $edit_record) {
+                        $error = true;
+                    } else {
+                        // Log the state after saving and write it to logging table
+                        $log->log_after($record['rid']);
+                        $log->write();
+                    }
+                }
             }
 
-            $edit_record = DnsRecord::edit_record($record);
-            if (false === $edit_record) {
-                $error = true;
+            if ($iface_zone_comments) {
+                DnsRecord::edit_zone_comment($_GET['id'], $_POST['comment']);
+            }
+
+            if (false === $error) {
+                DnsRecord::update_soa_serial($_GET['id']);
+
+                if ($one_record_changed) {
+                    success(SUC_ZONE_UPD);
+                } else {
+                    success(SUC_ZONE_NOCHANGE);
+                }
+
+                if ($pdnssec_use) {
+                    if (Dnssec::dnssec_rectify_zone($_GET['id'])) {
+                        success(SUC_EXEC_PDNSSEC_RECTIFY_ZONE);
+                    }
+                }
             } else {
-                // Log the state after saving and write it to logging table
-                $log->log_after($record['rid']);
-                $log->write();
+                error(ERR_ZONE_UPD);
             }
         }
-    }
 
-    if ($iface_zone_comments) {
-        DnsRecord::edit_zone_comment($_GET['id'], $_POST['comment']);
-    }
+        if (isset($_POST['save_as'])) {
+            if (ZoneTemplate::zone_templ_name_exists($_POST['templ_name'])) {
+                error(ERR_ZONE_TEMPL_EXIST);
+            } elseif ($_POST['templ_name'] == '') {
+                error(ERR_ZONE_TEMPL_IS_EMPTY);
+            } else {
+                success(SUC_ZONE_TEMPL_ADD);
+                $records = DnsRecord::get_records_from_domain_id($zone_id);
+                ZoneTemplate::add_zone_templ_save_as($_POST['templ_name'], $_POST['templ_descr'], $_SESSION['userid'], $records, DnsRecord::get_domain_name_by_id($zone_id));
+            }
+        }
 
-    if (false === $error) {
-        DnsRecord::update_soa_serial($_GET['id']);
+        $perm_view = Permission::getViewPermission();
+        $perm_edit = Permission::getEditPermission();
 
-        if ($one_record_changed) {
-            success(SUC_ZONE_UPD);
+        if (do_hook('verify_permission', 'zone_meta_edit_others')) {
+            $perm_meta_edit = "all";
+        } elseif (do_hook('verify_permission', 'zone_meta_edit_own')) {
+            $perm_meta_edit = "own";
         } else {
-            success(SUC_ZONE_NOCHANGE);
+            $perm_meta_edit = "none";
         }
 
-        if ($pdnssec_use) {
-            if (Dnssec::dnssec_rectify_zone($_GET['id'])) {
-                success(SUC_EXEC_PDNSSEC_RECTIFY_ZONE);
+        $perm_zone_master_add = do_hook('verify_permission', 'zone_master_add');
+        $perm_zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
+
+        $user_is_zone_owner = do_hook('verify_user_is_owner_zoneid', $zone_id);
+
+        $meta_edit = $perm_meta_edit == "all" || ($perm_meta_edit == "own" && $user_is_zone_owner == "1");
+
+        (do_hook('verify_permission', 'user_view_others')) ? $perm_view_others = "1" : $perm_view_others = "0";
+
+        if (isset($_POST['slave_master_change']) && is_numeric($_POST["domain"])) {
+            DnsRecord::change_zone_slave_master($_POST['domain'], $_POST['new_master']);
+        }
+
+        $types = ZoneType::getTypes();
+        if (isset($_POST['type_change']) && in_array($_POST['newtype'], $types)) {
+            DnsRecord::change_zone_type($_POST['newtype'], $zone_id);
+        }
+        if (isset($_POST["newowner"]) && is_numeric($_POST["domain"]) && is_numeric($_POST["newowner"])) {
+            DnsRecord::add_owner_to_zone($_POST["domain"], $_POST["newowner"]);
+        }
+        if (isset($_POST["delete_owner"]) && is_numeric($_POST["delete_owner"])) {
+            DnsRecord::delete_owner_from_zone($zone_id, $_POST["delete_owner"]);
+        }
+        if (isset($_POST["template_change"])) {
+            if (!isset($_POST['zone_template']) || "none" == $_POST['zone_template']) {
+                $new_zone_template = 0;
+            } else {
+                $new_zone_template = $_POST['zone_template'];
+            }
+            if ($_POST['current_zone_template'] != $new_zone_template) {
+                DnsRecord::update_zone_records($zone_id, $new_zone_template);
             }
         }
-    } else {
-        error(ERR_ZONE_UPD);
+
+        if ($perm_view == "none" || $perm_view == "own" && $user_is_zone_owner == "0") {
+            error(ERR_PERM_VIEW_ZONE);
+            include_once("inc/footer.inc.php");
+            exit();
+        }
+
+        if (DnsRecord::zone_id_exists($zone_id) == "0") {
+            error(ERR_ZONE_NOT_EXIST);
+            include_once("inc/footer.inc.php");
+            exit();
+        }
+
+        $zone_name = DnsRecord::get_domain_name_by_id($zone_id);
+
+        if (isset($_POST['sign_zone'])) {
+            DnsRecord::update_soa_serial($zone_id);
+            Dnssec::dnssec_secure_zone($zone_name);
+            Dnssec::dnssec_rectify_zone($zone_id);
+        }
+
+        if (isset($_POST['unsign_zone'])) {
+            Dnssec::dnssec_unsecure_zone($zone_name);
+            DnsRecord::update_soa_serial($zone_id);
+        }
+
+        $domain_type = DnsRecord::get_domain_type($zone_id);
+        $record_count = DnsRecord::count_zone_records($zone_id);
+        $zone_templates = ZoneTemplate::get_list_zone_templ($_SESSION['userid']);
+        $zone_template_id = DnsRecord::get_zone_template($zone_id);
+        $zone_template_details = ZoneTemplate::get_zone_templ_details($zone_template_id);
+        $slave_master = DnsRecord::get_domain_slave_master($zone_id);
+        $users = do_hook('show_users');
+
+        $zone_comment = '';
+        $raw_zone_comment = DnsRecord::get_zone_comment($zone_id);
+        if ($raw_zone_comment) { $zone_comment = htmlspecialchars($raw_zone_comment); }
+
+        $zone_name_to_display = DnsRecord::get_domain_name_by_id($zone_id);
+        if (preg_match("/^xn--/", $zone_name_to_display)) {
+            $idn_zone_name = idn_to_utf8($zone_name_to_display, IDNA_NONTRANSITIONAL_TO_ASCII);
+        } else {
+            $idn_zone_name = "";
+        }
+        $records = DnsRecord::get_records_from_domain_id($zone_id, $row_start, $iface_rowamount, $record_sort_by);
+        $owners = DnsRecord::get_users_from_domain_id($zone_id);
+
+        $this->render('edit.html', [
+            'zone_id' => $zone_id,
+            'zone_name' => $zone_name,
+            'zone_name_to_display' => $zone_name_to_display,
+            'idn_zone_name' => $idn_zone_name,
+            'zone_comment' => $zone_comment,
+            'domain_type' => $domain_type,
+            'record_count' => $record_count,
+            'zone_templates' => $zone_templates,
+            'zone_template_id' => $zone_template_id,
+            'zone_template_details' => $zone_template_details,
+            'slave_master' => $slave_master,
+            'users' => $users,
+            'owners' => $owners,
+            'records' => $records,
+            'perm_view' => $perm_view,
+            'perm_edit' => $perm_edit,
+            'perm_meta_edit' => $perm_meta_edit,
+            'meta_edit' => $meta_edit,
+            'perm_zone_master_add' => $perm_zone_master_add,
+            'perm_zone_slave_add' => $perm_zone_slave_add,
+            'perm_view_others' => $perm_view_others,
+            'user_is_zone_owner' => $user_is_zone_owner,
+            'zone_types' => $types,
+            'row_start' => $row_start,
+            'row_amount' => $iface_rowamount,
+            'record_sort_by' => $record_sort_by,
+            'pagination' => show_pages($record_count, $iface_rowamount, $zone_id),
+            'is_secured' => Dnssec::dnssec_is_zone_secured($zone_name),
+            'session_userid' => $_SESSION["userid"],
+            'dns_ttl' => $this->config('dns_ttl'),
+            'is_rev_zone' => preg_match('/i(p6|n-addr).arpa/i', $zone_name),
+            'record_types' => RecordType::getTypes(),
+        ]);
     }
 }
 
-if (isset($_POST['save_as'])) {
-    if (ZoneTemplate::zone_templ_name_exists($_POST['templ_name'])) {
-        error(ERR_ZONE_TEMPL_EXIST);
-    } elseif ($_POST['templ_name'] == '') {
-        error(ERR_ZONE_TEMPL_IS_EMPTY);
-    } else {
-        success(SUC_ZONE_TEMPL_ADD);
-        $records = DnsRecord::get_records_from_domain_id($zone_id);
-        ZoneTemplate::add_zone_templ_save_as($_POST['templ_name'], $_POST['templ_descr'], $_SESSION['userid'], $records, DnsRecord::get_domain_name_by_id($zone_id));
-    }
-}
-
-$perm_view = Permission::getViewPermission();
-$perm_edit = Permission::getEditPermission();
-
-if (do_hook('verify_permission', 'zone_meta_edit_others')) {
-    $perm_meta_edit = "all";
-} elseif (do_hook('verify_permission', 'zone_meta_edit_own')) {
-    $perm_meta_edit = "own";
-} else {
-    $perm_meta_edit = "none";
-}
-
-$perm_zone_master_add = do_hook('verify_permission', 'zone_master_add');
-$perm_zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
-
-$user_is_zone_owner = do_hook('verify_user_is_owner_zoneid', $zone_id);
-
-$meta_edit = $perm_meta_edit == "all" || ($perm_meta_edit == "own" && $user_is_zone_owner == "1");
-
-(do_hook('verify_permission', 'user_view_others')) ? $perm_view_others = "1" : $perm_view_others = "0";
-
-if (isset($_POST['slave_master_change']) && is_numeric($_POST["domain"])) {
-    DnsRecord::change_zone_slave_master($_POST['domain'], $_POST['new_master']);
-}
-
-$types = ZoneType::getTypes();
-if (isset($_POST['type_change']) && in_array($_POST['newtype'], $types)) {
-    DnsRecord::change_zone_type($_POST['newtype'], $zone_id);
-}
-if (isset($_POST["newowner"]) && is_numeric($_POST["domain"]) && is_numeric($_POST["newowner"])) {
-    DnsRecord::add_owner_to_zone($_POST["domain"], $_POST["newowner"]);
-}
-if (isset($_POST["delete_owner"]) && is_numeric($_POST["delete_owner"])) {
-    DnsRecord::delete_owner_from_zone($zone_id, $_POST["delete_owner"]);
-}
-if (isset($_POST["template_change"])) {
-    if (!isset($_POST['zone_template']) || "none" == $_POST['zone_template']) {
-        $new_zone_template = 0;
-    } else {
-        $new_zone_template = $_POST['zone_template'];
-    }
-    if ($_POST['current_zone_template'] != $new_zone_template) {
-        DnsRecord::update_zone_records($zone_id, $new_zone_template);
-    }
-}
-
-if ($perm_view == "none" || $perm_view == "own" && $user_is_zone_owner == "0") {
-    error(ERR_PERM_VIEW_ZONE);
-    include_once("inc/footer.inc.php");
-    exit();
-}
-
-if (DnsRecord::zone_id_exists($zone_id) == "0") {
-    error(ERR_ZONE_NOT_EXIST);
-    include_once("inc/footer.inc.php");
-    exit();
-}
-
-$zone_name = DnsRecord::get_domain_name_by_id($zone_id);
-
-if (isset($_POST['sign_zone'])) {
-    DnsRecord::update_soa_serial($zone_id);
-    Dnssec::dnssec_secure_zone($zone_name);
-    Dnssec::dnssec_rectify_zone($zone_id);
-}
-
-if (isset($_POST['unsign_zone'])) {
-    Dnssec::dnssec_unsecure_zone($zone_name);
-    DnsRecord::update_soa_serial($zone_id);
-}
-
-$domain_type = DnsRecord::get_domain_type($zone_id);
-$record_count = DnsRecord::count_zone_records($zone_id);
-$zone_templates = ZoneTemplate::get_list_zone_templ($_SESSION['userid']);
-$zone_template_id = DnsRecord::get_zone_template($zone_id);
-$zone_template_details = ZoneTemplate::get_zone_templ_details($zone_template_id);
-$slave_master = DnsRecord::get_domain_slave_master($zone_id);
-$users = do_hook('show_users');
-
-$zone_comment = '';
-$raw_zone_comment = DnsRecord::get_zone_comment($zone_id);
-if ($raw_zone_comment) { $zone_comment = htmlspecialchars($raw_zone_comment); }
-
-$zone_name_to_display = DnsRecord::get_domain_name_by_id($zone_id);
-if (preg_match("/^xn--/", $zone_name_to_display)) {
-    $idn_zone_name = idn_to_utf8($zone_name_to_display, IDNA_NONTRANSITIONAL_TO_ASCII);
-} else {
-    $idn_zone_name = "";
-}
-$records = DnsRecord::get_records_from_domain_id($zone_id, $row_start, $iface_rowamount, $record_sort_by);
-$owners = DnsRecord::get_users_from_domain_id($zone_id);
-
-$app->render('edit.html', [
-    'zone_id' => $zone_id,
-    'zone_name' => $zone_name,
-    'zone_name_to_display' => $zone_name_to_display,
-    'idn_zone_name' => $idn_zone_name,
-    'zone_comment' => $zone_comment,
-    'domain_type' => $domain_type,
-    'record_count' => $record_count,
-    'zone_templates' => $zone_templates,
-    'zone_template_id' => $zone_template_id,
-    'zone_template_details' => $zone_template_details,
-    'slave_master' => $slave_master,
-    'users' => $users,
-    'owners' => $owners,
-    'records' => $records,
-    'perm_view' => $perm_view,
-    'perm_edit' => $perm_edit,
-    'perm_meta_edit' => $perm_meta_edit,
-    'meta_edit' => $meta_edit,
-    'perm_zone_master_add' => $perm_zone_master_add,
-    'perm_zone_slave_add' => $perm_zone_slave_add,
-    'perm_view_others' => $perm_view_others,
-    'user_is_zone_owner' => $user_is_zone_owner,
-    'zone_types' => $types,
-    'row_start' => $row_start,
-    'row_amount' => $iface_rowamount,
-    'record_sort_by' => $record_sort_by,
-    'pagination' => show_pages($record_count, $iface_rowamount, $zone_id),
-    'is_secured' => Dnssec::dnssec_is_zone_secured($zone_name),
-    'session_userid' => $_SESSION["userid"],
-    'dns_ttl' => $app->config('dns_ttl'),
-    'is_rev_zone' => preg_match('/i(p6|n-addr).arpa/i', $zone_name),
-    'record_types' => RecordType::getTypes(),
-]);
-
-include_once("inc/footer.inc.php");
+$controller = new EditController();
+$controller->run();
