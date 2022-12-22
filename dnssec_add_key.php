bb26a03d9e3bc953b27977de2ac758afec67e717
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-22 16:56:40 +0200

    Code converted to MVC pattern.

--- dnssec_add_key.php
+++ dnssec_add_key.php
@@ -29,7 +29,6 @@
  * @license     https://opensource.org/licenses/GPL-3.0 GPL
  */
 
-use Poweradmin\AppFactory;
 use Poweradmin\DnsRecord;
 use Poweradmin\Dnssec;
 use Poweradmin\Validation;
@@ -37,80 +36,85 @@ use Poweradmin\Validation;
 require_once 'inc/toolkit.inc.php';
 require_once 'inc/message.inc.php';
 
-include_once("inc/header.inc.php");
+class DnsSecAddKeyController extends \Poweradmin\BaseController {
 
-$zone_id = "-1";
-if (isset($_GET['id']) && Validation::is_number($_GET['id'])) {
-    $zone_id = htmlspecialchars($_GET['id']);
-}
+    public function run(): void
+    {
+        $zone_id = "-1";
+        if (isset($_GET['id']) && Validation::is_number($_GET['id'])) {
+            $zone_id = htmlspecialchars($_GET['id']);
+        }
 
-$user_is_zone_owner = do_hook('verify_user_is_owner_zoneid', $zone_id);
+        $user_is_zone_owner = do_hook('verify_user_is_owner_zoneid', $zone_id);
 
-if ($user_is_zone_owner == "0") {
-    error(ERR_PERM_VIEW_ZONE);
-    include_once("inc/footer.inc.php");
-    exit();
-}
+        if ($user_is_zone_owner == "0") {
+            error(ERR_PERM_VIEW_ZONE);
+            include_once("inc/footer.inc.php");
+            exit();
+        }
 
-if (DnsRecord::zone_id_exists($zone_id) == "0") {
-    error(ERR_ZONE_NOT_EXIST);
-    include_once("inc/footer.inc.php");
-    exit();
-}
+        if (DnsRecord::zone_id_exists($zone_id) == "0") {
+            error(ERR_ZONE_NOT_EXIST);
+            include_once("inc/footer.inc.php");
+            exit();
+        }
 
-$key_type = "";
-if (isset($_POST['key_type'])) {
-    $key_type = $_POST['key_type'];
+        $key_type = "";
+        if (isset($_POST['key_type'])) {
+            $key_type = $_POST['key_type'];
 
-    if ($key_type != 'ksk' && $key_type != 'zsk') {
-        error(ERR_INV_INPUT);
-        include_once("inc/footer.inc.php");
-        exit;
+            if ($key_type != 'ksk' && $key_type != 'zsk') {
+                error(ERR_INV_INPUT);
+                include_once("inc/footer.inc.php");
+                exit;
+            }
+        }
+
+        $bits = "";
+        if (isset($_POST["bits"])) {
+            $bits = $_POST["bits"];
+
+            $valid_values = array('2048', '1024', '768', '384', '256');
+            if (!in_array($bits, $valid_values)) {
+                error(ERR_INV_INPUT);
+                include_once("inc/footer.inc.php");
+                exit;
+            }
+        }
+
+        $algorithm = "";
+        if (isset($_POST["algorithm"])) {
+            $algorithm = $_POST["algorithm"];
+
+            // To check the supported DNSSEC algorithms in your build of PowerDNS, run pdnsutil list-algorithms.
+            $valid_algorithm = array('rsasha1', 'rsasha1-nsec3', 'rsasha256', 'rsasha512', 'ecdsa256', 'ecdsa384', 'ed25519', 'ed448');
+            if (!in_array($algorithm, $valid_algorithm)) {
+                error(ERR_INV_INPUT);
+                include_once("inc/footer.inc.php");
+                exit;
+            }
+        }
+
+        $domain_name = DnsRecord::get_domain_name_by_id($zone_id);
+        if (isset($_POST["submit"])) {
+            if (Dnssec::dnssec_add_zone_key($domain_name, $key_type, $bits, $algorithm)) {
+                $this->setMessage('dnssec', 'success', SUC_EXEC_PDNSSEC_ADD_ZONE_KEY);
+                $this->redirect('dnssec.php', [id => $zone_id]);
+            } else {
+                error(ERR_EXEC_PDNSSEC_ADD_ZONE_KEY);
+            }
+        }
+
+        $this->render('dnssec_add_key.html', [
+            'zone_id' => $zone_id,
+            'domain_name' => $domain_name,
+            'key_type' => $key_type,
+            'bits' => $bits,
+            'algorithm' => $algorithm,
+            'algorithm_names' => Dnssec::dnssec_algorithm_names(),
+        ]);
     }
 }
 
-$bits = "";
-if (isset($_POST["bits"])) {
-    $bits = $_POST["bits"];
-
-    $valid_values = array('2048', '1024', '768', '384', '256');
-    if (!in_array($bits, $valid_values)) {
-        error(ERR_INV_INPUT);
-        include_once("inc/footer.inc.php");
-        exit;
-    }
-}
-
-$algorithm = "";
-if (isset($_POST["algorithm"])) {
-    $algorithm = $_POST["algorithm"];
-
-    // To check the supported DNSSEC algorithms in your build of PowerDNS, run pdnsutil list-algorithms.
-    $valid_algorithm = array('rsasha1', 'rsasha1-nsec3', 'rsasha256', 'rsasha512', 'ecdsa256', 'ecdsa384', 'ed25519', 'ed448');
-    if (!in_array($algorithm, $valid_algorithm)) {
-        error(ERR_INV_INPUT);
-        include_once("inc/footer.inc.php");
-        exit;
-    }
-}
-
-$domain_name = DnsRecord::get_domain_name_by_id($zone_id);
-if (isset($_POST["submit"])) {
-    if (Dnssec::dnssec_add_zone_key($domain_name, $key_type, $bits, $algorithm)) {
-        success(SUC_EXEC_PDNSSEC_ADD_ZONE_KEY);
-    } else {
-        error(ERR_EXEC_PDNSSEC_ADD_ZONE_KEY);
-    }
-}
-
-$app = AppFactory::create();
-$app->render('dnssec_add_key.html', [
-    'zone_id' => $zone_id,
-    'domain_name' => $domain_name,
-    'key_type' => $key_type,
-    'bits' => $bits,
-    'algorithm' => $algorithm,
-    'algorithm_names' => Dnssec::dnssec_algorithm_names(),
-]);
-
-include_once("inc/footer.inc.php");
+$controller = new DnsSecAddKeyController();
+$controller->run();
