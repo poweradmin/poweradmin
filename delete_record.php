f63b3b283d7e10bc988f32476f17e23abc437559
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-22 06:34:40 +0200

    Use MVC like pattern.

--- delete_record.php
+++ delete_record.php
@@ -29,7 +29,7 @@
  * @license     https://opensource.org/licenses/GPL-3.0 GPL
  */
 
-use Poweradmin\AppFactory;
+use Poweradmin\BaseController;
 use Poweradmin\DnsRecord;
 use Poweradmin\Dnssec;
 use Poweradmin\Permission;
@@ -39,67 +39,74 @@ use Poweradmin\Logger;
 require_once 'inc/toolkit.inc.php';
 require_once 'inc/message.inc.php';
 
-include_once 'inc/header.inc.php';
+class DeleteRecordController extends BaseController {
 
-$app = AppFactory::create();
-$pdnssec_use = $app->config('pdnssec_use');
+    public function run(): void
+    {
+        if (!isset($_GET['id']) || !Validation::is_number($_GET['id'])) {
+            error(ERR_INV_INPUT);
+            include_once('inc/footer.inc.php');
+            exit;
+        }
+        $record_id = htmlspecialchars($_GET['id']);
 
-if (!isset($_GET['id']) || !Validation::is_number($_GET['id'])) {
-    error(ERR_INV_INPUT);
-    include_once('inc/footer.inc.php');
-    exit;
-}
-$record_id = htmlspecialchars($_GET['id']);
-
-$zid = DnsRecord::get_zone_id_from_record_id($record_id);
-if ($zid == NULL) {
-    header("Location: list_zones.php");
-    exit;
-}
-
-if (isset($_GET['confirm']) && Validation::is_number($_GET['confirm']) && $_GET['confirm'] == 1) {
-    $record_info = DnsRecord::get_record_from_id($record_id);
-    if (DnsRecord::delete_record($record_id)) {
-        success("<a href=\"edit.php?id=" . $zid . "\">" . SUC_RECORD_DEL . "</a>");
-        if (isset($record_info['prio'])) {
-            Logger::log_info(sprintf('client_ip:%s user:%s operation:delete_record record_type:%s record:%s content:%s ttl:%s priority:%s',
-                $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
-                $record_info['type'], $record_info['name'], $record_info['content'], $record_info['ttl'], $record_info['prio']), $zid);
-        } else {
-            Logger::log_info(sprintf('client_ip:%s user:%s operation:delete_record record_type:%s record:%s content:%s ttl:%s',
-                $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
-                $record_info['type'], $record_info['name'], $record_info['content'], $record_info['ttl']), $zid);
+        $zid = DnsRecord::get_zone_id_from_record_id($record_id);
+        if ($zid == NULL) {
+            header("Location: list_zones.php");
+            exit;
         }
 
-        DnsRecord::delete_record_zone_templ($record_id);
-        DnsRecord::update_soa_serial($zid);
+        if (isset($_GET['confirm'])) {
+            $record_info = DnsRecord::get_record_from_id($record_id);
+            if (DnsRecord::delete_record($record_id)) {
+                success("<a href=\"edit.php?id=" . $zid . "\">" . SUC_RECORD_DEL . "</a>");
+                if (isset($record_info['prio'])) {
+                    Logger::log_info(sprintf('client_ip:%s user:%s operation:delete_record record_type:%s record:%s content:%s ttl:%s priority:%s',
+                        $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
+                        $record_info['type'], $record_info['name'], $record_info['content'], $record_info['ttl'], $record_info['prio']), $zid);
+                } else {
+                    Logger::log_info(sprintf('client_ip:%s user:%s operation:delete_record record_type:%s record:%s content:%s ttl:%s',
+                        $_SERVER['REMOTE_ADDR'], $_SESSION["userlogin"],
+                        $record_info['type'], $record_info['name'], $record_info['content'], $record_info['ttl']), $zid);
+                }
 
-        // do also rectify-zone
-        if ($pdnssec_use && Dnssec::dnssec_rectify_zone($zid)) {
-            success(SUC_EXEC_PDNSSEC_RECTIFY_ZONE);
+                DnsRecord::delete_record_zone_templ($record_id);
+                DnsRecord::update_soa_serial($zid);
+
+                $pdnssec_use = $this->config('pdnssec_use');
+                if ($pdnssec_use && Dnssec::dnssec_rectify_zone($zid)) {
+                    success(SUC_EXEC_PDNSSEC_RECTIFY_ZONE);
+                }
+            }
+
+            include_once('inc/footer.inc.php');
+            exit;
         }
+
+        $perm_edit = Permission::getEditPermission();
+
+        $zone_info = DnsRecord::get_zone_info_from_id($zid);
+        $zone_id = DnsRecord::recid_to_domid($record_id);
+        $user_is_zone_owner = do_hook('verify_user_is_owner_zoneid' , $zone_id );
+        if ($zone_info['type'] == "SLAVE" || $perm_edit == "none" || ($perm_edit == "own" || $perm_edit == "own_as_client") && $user_is_zone_owner == "0") {
+            error(ERR_PERM_EDIT_RECORD);
+            include_once('inc/footer.inc.php');
+            exit;
+        }
+
+        $this->showQuestion($record_id, $zid, $zone_id);
     }
 
-    include_once('inc/footer.inc.php');
-    exit;
+    public function showQuestion(string $record_id, $zid, int $zone_id): void
+    {
+        $this->render('delete_record.html', [
+            'record_id' => $record_id,
+            'zid' => $zid,
+            'zone_name' => DnsRecord::get_domain_name_by_id($zone_id),
+            'record_info' => DnsRecord::get_record_from_id($record_id),
+        ]);
+    }
 }
 
-$perm_edit = Permission::getEditPermission();
-
-$zone_info = DnsRecord::get_zone_info_from_id($zid);
-$zone_id = DnsRecord::recid_to_domid($record_id);
-$user_is_zone_owner = do_hook('verify_user_is_owner_zoneid' , $zone_id );
-if ($zone_info['type'] == "SLAVE" || $perm_edit == "none" || ($perm_edit == "own" || $perm_edit == "own_as_client") && $user_is_zone_owner == "0") {
-    error(ERR_PERM_EDIT_RECORD);
-    include_once('inc/footer.inc.php');
-    exit;
-}
-
-$app->render('delete_record.html', [
-    'record_id' => $record_id,
-    'zid' => $zid,
-    'zone_name' => DnsRecord::get_domain_name_by_id($zone_id),
-    'record_info' => DnsRecord::get_record_from_id($record_id),
-]);
-
-include_once('inc/footer.inc.php');
+$controller = new DeleteRecordController();
+$controller->run();
