2d950b8592315bcbde2ccda83e1651a71871d667
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-28 11:16:31 +0200

    Updated permission checking.

--- list_zones.php
+++ list_zones.php
@@ -40,12 +40,11 @@ class ListZonesController extends BaseController {
 
     public function run(): void
     {
-        $perm_view_zone_own = do_hook('zone_content_view_own');
-        $perm_view_zone_others = do_hook('zone_content_view_others');
-        $perm_zone_master_add = do_hook('zone_master_add');
-        $perm_zone_slave_add = do_hook('zone_slave_add');
-        $perm_is_godlike = do_hook('verify_permission', 'user_is_ueberuser');
-        $permission_check = !$perm_is_godlike && !$perm_view_zone_own && !$perm_view_zone_others && !$perm_zone_master_add && !$perm_zone_slave_add;
+        $perm_view_zone_own = do_hook('verify_permission', 'zone_content_view_own');
+        $perm_view_zone_others = do_hook('verify_permission', 'zone_content_view_others');
+        $perm_zone_master_add = do_hook('verify_permission', 'zone_master_add');
+        $perm_zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
+        $permission_check = !$perm_view_zone_own && !$perm_view_zone_others && !$perm_zone_master_add && !$perm_zone_slave_add;
 
         $this->checkCondition($permission_check, _('You do not have sufficient permissions to view this page.'));
 
