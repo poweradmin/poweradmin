52a090ee24785494e88d159e887779e129a7a805
Edmondas Girkantas <edmondas@girkantas.lt>
2022-12-29 19:33:16 +0200

    Removed not required checks.

--- list_zones.php
+++ list_zones.php
@@ -42,10 +42,8 @@ class ListZonesController extends BaseController {
     {
         $perm_view_zone_own = do_hook('verify_permission', 'zone_content_view_own');
         $perm_view_zone_others = do_hook('verify_permission', 'zone_content_view_others');
-        $perm_zone_master_add = do_hook('verify_permission', 'zone_master_add');
-        $perm_zone_slave_add = do_hook('verify_permission', 'zone_slave_add');
-        $permission_check = !$perm_view_zone_own || !$perm_view_zone_others || !$perm_zone_master_add || !$perm_zone_slave_add;
 
+        $permission_check = !($perm_view_zone_own || $perm_view_zone_others);
         $this->checkCondition($permission_check, _('You do not have sufficient permissions to view this page.'));
 
         $this->listZones();
