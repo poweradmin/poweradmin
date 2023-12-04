ca24b54081a762e956ea748ef0dc62432b05e79f
Edmondas Girkantas <edmondas@girkantas.lt>
2023-12-04 20:07:16 +0200

    refactor: unify locale file naming in getLocaleFile function

--- install/helpers/locale_handler.php
+++ install/helpers/locale_handler.php
@@ -22,11 +22,11 @@
 
 use Poweradmin\Application\Presenter\ErrorPresenter;
 use Poweradmin\Domain\Error\ErrorMessage;
+
 function getLocaleFile(string $iface_lang): string
 {
     if (in_array($iface_lang, ['cs_CZ', 'de_DE', 'fr_FR', 'ja_JP', 'nb_NO', 'nl_NL', 'pl_PL', 'ru_RU', 'tr_TR', 'zh_CN'])) {
-        $short_locale = substr($iface_lang, 0, 2);
-        return dirname(__DIR__, 2) . "/locale/$iface_lang/LC_MESSAGES/$short_locale.po";
+        return dirname(__DIR__, 2) . "/locale/$iface_lang/LC_MESSAGES/messages.po";
     }
     return dirname(__DIR__, 2) . "/locale/en_EN/LC_MESSAGES/en.po";
 }
--- lib/Application.php
+++ lib/Application.php
@@ -100,8 +100,7 @@ class Application
     public function getLocaleFile(string $iface_lang): string
     {
         if (in_array($iface_lang, ['cs_CZ', 'de_DE', 'fr_FR', 'ja_JP', 'nb_NO', 'nl_NL', 'pl_PL', 'ru_RU', 'tr_TR', 'zh_CN'])) {
-            $short_locale = substr($iface_lang, 0, 2);
-            return "locale/$iface_lang/LC_MESSAGES/$short_locale.po";
+            return "locale/$iface_lang/LC_MESSAGES/messages.po";
         }
         return "locale/en_EN/LC_MESSAGES/en.po";
     }
