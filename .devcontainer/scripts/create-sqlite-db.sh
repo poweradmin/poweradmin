d43cecf97eaa26a441908f33456139bbff203614
Edmondas Girkantas <edmondas@girkantas.lt>
2024-08-18 21:32:16 +0300

    chore(devcontainer): add SQLite support and improve database setup

--- .devcontainer/scripts/create-sqlite-db.sh
+++ .devcontainer/scripts/create-sqlite-db.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-DB_FILE="/data/pdns.db"
+DB_PATH="/data/db"
+DB_FILE="$DB_PATH/powerdns.db"
+
+if [ ! -e $DB_PATH ]
+then
+	mkdir -p $DB_PATH
+	chmod 777 $DB_PATH
+fi
 
 if [ ! -f "$DB_FILE" ]; then
     echo "Creating new SQLite database..."
     sqlite3 "$DB_FILE" < /schema.sqlite3.sql
+    chmod 666 "$DB_FILE"
     echo "Database created successfully."
 else
     echo "Database already exists. Skipping creation."
