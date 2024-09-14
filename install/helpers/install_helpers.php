cdebfddc5117a6862a2f81c48194930c1368456e
Edmondas Girkantas <edmondas@girkantas.lt>
2024-09-14 09:04:03 +0300

    chore(install): remove unnecessary footer rendering during installation

--- install/helpers/install_helpers.php
+++ install/helpers/install_helpers.php
@@ -33,7 +33,6 @@ function checkConfigFile($current_step, $local_config_file, $twig): void
             return; // Allow last step to be shown
         } else {
             echo "<p class='alert alert-danger'>" . _('There is already a configuration file in place, so the installation will be skipped.') . "</p>";
-            echo $twig->render('footer.html');
             exit;
         }
     }
