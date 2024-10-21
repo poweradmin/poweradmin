66dee3bb9293be1db2fc01c723684dd52e834dcf
Edmondas Girkantas <edmondas@girkantas.lt>
2024-10-21 20:15:40 +0300

    fix(ui): Correct textarea auto-resize behavior for specific record types, related to #557 and #581

--- assets/textareaAutoResize.js
+++ assets/textareaAutoResize.js
@@ -1,14 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var textareas = document.querySelectorAll('textarea');
+function updateContentInput() {
+    const recordTypeSelect = document.getElementById('recordTypeSelect');
+    const contentInputContainer = document.getElementById('contentInputContainer');
+    const selectedType = recordTypeSelect.value;
+    const initialValue = contentInputContainer.dataset.initialValue || '';
 
-    textareas.forEach(function(textarea) {
-        textarea.addEventListener('input', function() {
+    const textareaTypes = [
+        'APL', 'CERT', 'CDNSKEY', 'CDS', 'CSYNC', 'DHCID', 'DLV', 'DNSKEY', 'DS', 'HTTPS', 'IPSECKEY', 'KEY', 'LUA',
+        'NAPTR', 'NSEC', 'NSEC3', 'NSEC3PARAM', 'OPENPGPKEY', 'RKEY', 'RRSIG', 'SIG', 'SMIMEA', 'SPF', 'SSHFP', 'SVCB',
+        'TLSA', 'TKEY', 'TSIG', 'TXT', 'URI', 'ZONEMD'
+    ];
+
+    if (textareaTypes.includes(selectedType)) {
+        contentInputContainer.innerHTML = `<textarea id="recordContent" class="form-control form-control-sm" name="content" rows="1" required>${initialValue}</textarea>`;
+    } else {
+        contentInputContainer.innerHTML = `<input id="recordContent" class="form-control form-control-sm" type="text" name="content" value="${initialValue}" required>`;
+    }
+
+    const recordContent = document.getElementById('recordContent');
+    if (recordContent.tagName.toLowerCase() === 'textarea') {
+        recordContent.addEventListener('input', function() {
             this.style.height = 'auto';
             this.style.height = this.scrollHeight + 'px';
         });
 
-        if (textarea.value !== '') {
-            textarea.dispatchEvent(new Event('input'));
+        if (recordContent.value !== '') {
+            recordContent.style.height = 'auto';
+            recordContent.style.height = recordContent.scrollHeight + 'px';
         }
-    });
+    } else {
+        recordContent.addEventListener('input', function() {
+            this.style.height = 'auto';
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contentInputContainer = document.getElementById('contentInputContainer');
+    contentInputContainer.dataset.initialValue = document.getElementById('recordContent').value;
+    updateContentInput();
+    document.getElementById('recordTypeSelect').addEventListener('change', updateContentInput);
 });
