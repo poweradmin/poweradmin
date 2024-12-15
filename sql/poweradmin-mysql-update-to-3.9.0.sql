65d1c063c112d41bd5d6aaeca8abf287e5237225
Edmondas Girkantas <edmondas@girkantas.lt>
2024-12-15 20:27:53 +0200

    chore(migrations): Prepare update scripts for zone template records

---
+++ sql/poweradmin-mysql-update-to-3.9.0.sql
@@ -0,0 +1,1 @@
+ALTER TABLE zone_templ_records MODIFY COLUMN content varchar(2048) NOT NULL;
---
+++ sql/poweradmin-pgsql-update-to-3.9.0.sql
@@ -0,0 +1,1 @@
+ALTER TABLE zone_templ_records ALTER COLUMN content TYPE varchar(2048), ALTER COLUMN content SET NOT NULL;
