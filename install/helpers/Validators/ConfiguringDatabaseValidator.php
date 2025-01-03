f1f9d10fe9bbc969441f0bb11fd92186948f8857
Edmondas Girkantas <edmondas@girkantas.lt>
2025-01-03 19:04:48 +0200

    fix(install): add charset validation for MySQL and PostgreSQL databases

--- install/helpers/Validators/ConfiguringDatabaseValidator.php
+++ install/helpers/Validators/ConfiguringDatabaseValidator.php
@@ -44,8 +44,8 @@ class ConfiguringDatabaseValidator extends BaseValidator
                 'step' => [
                     new Assert\NotBlank(),
                     new Assert\EqualTo([
-                        'value' => InstallationSteps::STEP_CONFIGURING_DATABASE,
-                        'message' => 'The step must be equal to ' . InstallationSteps::STEP_CONFIGURING_DATABASE
+                        'value' => InstallationSteps::STEP_SETUP_ACCOUNT_AND_NAMESERVERS,
+                        'message' => 'The step must be equal to ' . InstallationSteps::STEP_SETUP_ACCOUNT_AND_NAMESERVERS
                     ])
                 ],
                 'db_type' => [
@@ -308,9 +308,51 @@ class ConfiguringDatabaseValidator extends BaseValidator
         }
     }
 
-    public function validateDbCharset($charset, ExecutionContextInterface $context): void
+    public function validateDbCharset(string $charset, ExecutionContextInterface $context): void
     {
-        // Implementation needed - requires supported charset list
+        $charset = strtolower($charset);
+
+        $input = $context->getRoot();
+
+        if ($input['db_type'] === 'mysql') {
+            $validCharsets = [
+                'utf8', 'utf8mb4', 'latin1', 'ascii', 'binary',
+                'utf16', 'utf16le', 'utf32', 'big5', 'dec8',
+                'cp850', 'hp8', 'koi8r', 'latin2', 'swe7',
+                'ascii', 'ujis', 'sjis', 'hebrew', 'tis620',
+                'euckr', 'koi8u', 'gb2312', 'greek', 'cp1250',
+                'gbk', 'latin5', 'armscii8', 'cp866', 'keybcs2',
+                'macce', 'macroman', 'cp852', 'latin7', 'cp1251',
+                'cp1256', 'cp1257', 'binary', 'geostd8', 'cp932',
+                'eucjpms'
+            ];
+        } else if ($input['db_type'] === 'pgsql') {
+            $validCharsets = [
+                'utf8', 'unicode', 'win', 'win1250', 'win1251',
+                'win1252', 'win1253', 'win1254', 'win1255', 'win1256',
+                'win1257', 'win1258', 'win866', 'win874', 'iso_8859_1',
+                'iso_8859_2', 'iso_8859_3', 'iso_8859_4', 'iso_8859_5',
+                'iso_8859_6', 'iso_8859_7', 'iso_8859_8', 'iso_8859_9',
+                'iso_8859_10', 'iso_8859_13', 'iso_8859_14', 'iso_8859_15',
+                'iso_8859_16', 'koi8', 'koi8r', 'koi8u', 'latin1', 'latin2',
+                'latin3', 'latin4', 'latin5', 'latin6', 'latin7', 'latin8',
+                'latin9', 'latin10', 'sql_ascii', 'euc_jp', 'euc_cn',
+                'euc_kr', 'euc_tw', 'gb18030', 'gbk', 'johab', 'sjis',
+                'big5'
+            ];
+        } else {
+            $context->buildViolation('Unsupported database type: {{ type }}')
+                ->setParameter('{{ type }}', $input['db_type'])
+                ->addViolation();
+            return;
+        }
+
+        if (!in_array($charset, $validCharsets, true)) {
+            $context->buildViolation('Invalid character set "{{ charset }}" for {{ type }}')
+                ->setParameter('{{ charset }}', $charset)
+                ->setParameter('{{ type }}', $input['db_type'])
+                ->addViolation();
+        }
     }
 
     public function validateDbCollation($collation, ExecutionContextInterface $context): void
