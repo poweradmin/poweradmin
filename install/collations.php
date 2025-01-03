753816d794fc95b188eab069a2bf5c956c369122
Edmondas Girkantas <edmondas@girkantas.lt>
2025-01-03 22:04:27 +0200

    refactor(validation): extract charset and collation config into separate files

---
+++ install/collations.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'mysql' => [
+        'utf8_general_ci', 'utf8_unicode_ci', 'utf8_bin', 'utf8mb4_general_ci', 'utf8mb4_unicode_ci', 'utf8mb4_bin',
+        'latin1_swedish_ci', 'latin1_general_ci', 'latin1_general_cs', 'ascii_general_ci', 'ascii_bin',
+        'binary', 'cp1250_general_ci', 'cp1250_bin', 'cp1250_croatian_ci', 'geostd8_general_ci',
+        'latin2_general_ci', 'latin2_hungarian_ci', 'latin5_turkish_ci', 'armscii8_general_ci', 'cp1251_general_ci',
+        'cp1251_bin', 'cp1256_general_ci', 'cp866_general_ci', 'cp866_bin', 'hp8_english_ci',
+        'keybcs2_general_ci', 'keybcs2_bin', 'koi8_general_ci', 'koi8_bin', 'macce_general_ci',
+        'macroman_general_ci', 'swe7_swedish_ci', 'tis620_thai_ci', 'euckr_korean_ci', 'ucs2_general_ci',
+        'ucs2_bin', 'utf16_general_ci', 'utf16_bin', 'utf32_general_ci', 'utf32_bin'
+    ],
+    'pgsql' => [
+        'C', 'POSIX', 'en_US.utf8', 'de_DE.utf8', 'fr_FR.utf8',
+        'es_ES.utf8', 'it_IT.utf8', 'ja_JP.utf8', 'ko_KR.utf8', 'zh_CN.utf8',
+        'zh_TW.utf8', 'ru_RU.utf8', 'pl_PL.utf8', 'pt_BR.utf8', 'cs_CZ.utf8',
+        'tr_TR.utf8', 'sv_SE.utf8', 'fi_FI.utf8', 'da_DK.utf8', 'no_NO.utf8',
+        'nl_NL.utf8', 'hu_HU.utf8', 'he_IL.utf8', 'ar_SA.utf8', 'el_GR.utf8',
+        'bg_BG.utf8', 'uk_UA.utf8', 'sk_SK.utf8', 'sl_SI.utf8', 'lv_LV.utf8',
+        'lt_LT.utf8', 'et_EE.utf8'
+    ],
+];
