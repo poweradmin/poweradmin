2c13d20153c5d47e17508cff0572c3503cf63f41
Edmondas Girkantas <edmondas@girkantas.lt>
2025-06-15 22:44:27 +0300

    chore(tests): add sample inserts for mixed case domains issue

---
+++ tests/sql/mixed-case-domains.sql
@@ -0,0 +1,26 @@
+-- Insert domains starting with uppercase T
+INSERT INTO domains (name, type) VALUES ('Test1.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test2.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test3.com', 'MASTER');
+
+-- Insert domains starting with lowercase t
+INSERT INTO domains (name, type) VALUES ('test4.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('test5.com', 'MASTER');
+
+-- Insert domains starting with uppercase T
+INSERT INTO domains (name, type) VALUES ('Test11.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test12.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test13.com', 'MASTER');
+
+-- Insert domains starting with lowercase t
+INSERT INTO domains (name, type) VALUES ('test14.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('test15.com', 'MASTER');
+
+-- Insert domains starting with uppercase T
+INSERT INTO domains (name, type) VALUES ('Test21.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test22.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('Test23.com', 'MASTER');
+
+-- Insert domains starting with lowercase t
+INSERT INTO domains (name, type) VALUES ('test24.com', 'MASTER');
+INSERT INTO domains (name, type) VALUES ('test25.com', 'MASTER');
