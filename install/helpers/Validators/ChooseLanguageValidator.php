d5f5acb5051543e475254848fb3c0bcd4a994787
Edmondas Girkantas <edmondas@girkantas.lt>
2025-06-20 21:18:07 +0300

    fix(install): add GET request validation support to ChooseLanguageValidator

--- install/helpers/Validators/ChooseLanguageValidator.php
+++ install/helpers/Validators/ChooseLanguageValidator.php
@@ -3,12 +3,32 @@
 namespace PoweradminInstall\Validators;
 
 use PoweradminInstall\InstallationSteps;
+use PoweradminInstall\LocaleHandler;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ChooseLanguageValidator extends BaseValidator
 {
     public function validate(): array
     {
+        // For GET requests (like "Check Again"), we don't need to validate the same constraints
+        if ($this->request->isMethod('GET')) {
+            // Only validate language for GET requests
+            $constraints = new Assert\Collection([
+                'language' => [
+                    new Assert\NotBlank(),
+                    new Assert\Choice(['choices' => LocaleHandler::getAvailableLanguages()]),
+                ],
+                'step' => [
+                    new Assert\NotBlank(),
+                ],
+            ]);
+
+            $input = $this->request->query->all();
+            $violations = $this->validator->validate($input, $constraints);
+            return ValidationErrorHelper::formatErrors($violations);
+        }
+
+        // For POST requests, use the full validation
         $constraints = new Assert\Collection(array_merge(
             $this->getBaseConstraints(),
             [
