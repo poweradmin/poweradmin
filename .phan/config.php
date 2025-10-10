b392514c1bbed6e1771b94b35479053d811203bb
Edmondas Girkantas <edmondas@girkantas.lt>
2025-10-10 18:54:12 +0300

    chore(phan): suppress deprecated class warnings

--- .phan/config.php
+++ .phan/config.php
@@ -260,7 +260,9 @@ return [
 
     // Add any issue types (such as `'PhanUndeclaredMethod'`)
     // to this list to inhibit them from being reported.
-    'suppress_issue_types' => [],
+    'suppress_issue_types' => [
+        'PhanDeprecatedClass',
+    ],
 
     // A regular expression to match files to be excluded
     // from parsing and analysis and will not be read at all.
