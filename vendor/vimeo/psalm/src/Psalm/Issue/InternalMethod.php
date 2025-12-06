12b73bec6cdc75caf7fc0056247d052bb4ca8b74
Edmondas Girkantas <edmondas@girkantas.lt>
2025-12-06 15:55:01 +0200

    chore(deps): regenerate vendor autoload for production

--- vendor/vimeo/psalm/src/Psalm/Issue/InternalMethod.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Psalm\Issue;
-
-final class InternalMethod extends MethodIssue
-{
-    public const ERROR_LEVEL = 4;
-    public const SHORTCODE = 175;
-}
--- vendor/vimeo/psalm/src/Psalm/Issue/PossiblyUndefinedGlobalVariable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Psalm\Issue;
-
-final class PossiblyUndefinedGlobalVariable extends VariableIssue
-{
-    public const ERROR_LEVEL = 3;
-    public const SHORTCODE = 126;
-}
