cb0e0bb00f3c1c547c1a9c8ceb696faa2df84a06
Edmondas Girkantas <edmondas@girkantas.lt>
2026-01-07 15:21:25 +0200

    chore: bump version to 4.2.0

--- lib/Version.php
+++ lib/Version.php
@@ -4,7 +4,7 @@
  *  See <https://www.poweradmin.org> for more details.
  *
  *  Copyright 2007-2010 Rejo Zenger <rejo@zenger.nl>
- *  Copyright 2010-2025 Poweradmin Development Team
+ *  Copyright 2010-2026 Poweradmin Development Team
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -34,5 +34,5 @@ class Version
      *
      * @var string
      */
-    public final const VERSION = '4.1.0';
+    public final const VERSION = '4.2.0';
 }
