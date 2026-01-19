dc17c0c0d3b3016a992a7f10e6504db64789a2dc
Edmondas Girkantas <edmondas@girkantas.lt>
2026-01-19 20:43:15 +0200

    test: skip installer tests when install folder not present

--- tests/unit/TemplateUtilsTest.php
+++ tests/unit/TemplateUtilsTest.php
@@ -3,103 +3,110 @@
 namespace unit;
 
 use PHPUnit\Framework\TestCase;
-use PoweradminInstall\StepValidator;
 
 class TemplateUtilsTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        // Skip tests if install folder doesn't exist
+        if (!class_exists('PoweradminInstall\StepValidator')) {
+            $this->markTestSkipped('Install folder not present - StepValidator class not available');
+        }
+    }
+
     public function testCanGetStepFromValidInput(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(3, $stepValidator->getCurrentStep(3));
     }
 
     public function testCanReturnDefaultStepWhenInputIsEmpty(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(null));
     }
 
     public function testCanHandleNonNumericStep(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('non-numeric'));
     }
 
     public function testGetCurrentStepWithVeryLargeNumber(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('999999999999999999999999'));
     }
 
     public function testGetCurrentStepWithNegativeNumber(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(-5));
     }
 
     public function testGetCurrentStepWithZero(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(0));
     }
 
     public function testGetCurrentStepWithFloat(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(3.5));
     }
 
     public function testGetCurrentStepWithStringNumber(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(5, $stepValidator->getCurrentStep('5'));
     }
 
     public function testGetCurrentStepWithNonAsciiNumbers(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('٣'));
     }
 
     public function testGetCurrentStepWithInjection(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('<script>alert("test")</script>'));
     }
 
     public function testGetCurrentStepWithArray(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(['1', '2']));
     }
 
     public function testGetCurrentStepWithLeadingWhitespace(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(' 42'));
     }
 
     public function testGetCurrentStepWithTrailingWhitespace(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('42 '));
     }
 
     public function testGetCurrentStepWithInternalWhitespace(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('4 2'));
     }
 
     public function testGetCurrentStepWithEmptyString(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep(''));
     }
 
     public function testGetCurrentStepWithSpecialCharacters(): void
     {
-        $stepValidator = new StepValidator();
+        $stepValidator = new \PoweradminInstall\StepValidator();
         $this->assertEquals(1, $stepValidator->getCurrentStep('4@2'));
     }
 }
