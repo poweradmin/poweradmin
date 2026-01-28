43551882733b5a99214d0f19f3c4e0bc3d828dd5
Edmondas Girkantas <edmondas@girkantas.lt>
2026-01-28 14:46:35 +0200

    fix(deps): update phpunit to 10.5.62+ to address CVE-2026-24765

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPMD\\' => array($vendorDir . '/phpmd/phpmd/src/main/php'),
-    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
 );
