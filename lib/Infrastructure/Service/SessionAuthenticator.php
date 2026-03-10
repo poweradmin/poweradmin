983a9fbb8fcffc1595efd72c8ce0cf560e483e17
Edmondas Girkantas <edmondas@girkantas.lt>
2026-03-10 21:26:37 +0200

    style: replace inline FQCNs with use imports

--- lib/Infrastructure/Service/SessionAuthenticator.php
+++ lib/Infrastructure/Service/SessionAuthenticator.php
@@ -4,7 +4,7 @@
  *  See <https://www.poweradmin.org> for more details.
  *
  *  Copyright 2007-2010 Rejo Zenger <rejo@zenger.nl>
- *  Copyright 2010-2025 Poweradmin Development Team
+ *  Copyright 2010-2026 Poweradmin Development Team
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -77,7 +77,7 @@ class SessionAuthenticator extends LoggingService
         $this->loginAttemptService = new LoginAttemptService($connection, $this->configManager);
         $this->recaptchaService = new RecaptchaService($configManager);
 
-        $userContextService = new \Poweradmin\Domain\Service\UserContextService();
+        $userContextService = new UserContextService();
         $this->ldapAuthenticator = new LdapAuthenticator(
             $connection,
             $configManager,
