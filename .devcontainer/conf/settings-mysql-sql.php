cbc95329b047133fbd729ef076de8ba8fe17c11b
Edmondas Girkantas <edmondas@girkantas.lt>
2026-03-31 18:39:39 +0300

    chore(devcontainer): enable search_group_records in all database settings

--- .devcontainer/conf/settings-mysql-sql.php
+++ .devcontainer/conf/settings-mysql-sql.php
@@ -64,6 +64,7 @@ return [
         'show_zone_comments' => false,
         'enable_consistency_checks' => true,
         'add_reverse_record' => true,
+        'search_group_records' => true,
     ],
 
     /**
