dd8a88d3305f589536497e4020f68597fc5ad72c
Edmondas Girkantas <edmondas@girkantas.lt>
2026-04-02 19:33:17 +0300

    fix(dns): remove duplicate SOA serial increment on inline record add (closes #1122)

--- lib/Application/Controller/EditController.php
+++ lib/Application/Controller/EditController.php
@@ -983,9 +983,6 @@ class EditController extends BaseController
             $this->setMessage('edit', 'success', _('The record was successfully added.'));
         }
 
-        // Update the zone's SOA serial
-        $this->dnsRecord->updateSOASerial($zone_id);
-
         return true;
     }
 
