eb9391882a78685de12786c22c75bc9259bc849c
Edmondas Girkantas <edmondas@girkantas.lt>
2026-09-16 20:25:46 +0300

    fix(users): gate a user's template assignment on user_edit_templ_perm

--- lib/Domain/Model/UserManager.php
+++ lib/Domain/Model/UserManager.php
@@ -4,7 +4,7 @@
  *  See <https://www.poweradmin.org> for more details.
  *
  *  Copyright 2007-2010 Rejo Zenger <rejo@zenger.nl>
- *  Copyright 2010-2025 Poweradmin Development Team
+ *  Copyright 2010-2026 Poweradmin Development Team
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -1038,7 +1038,7 @@ class UserManager
     {
         $perm_edit_own = self::verifyPermission($this->db, 'user_edit_own');
         $perm_edit_others = self::verifyPermission($this->db, 'user_edit_others');
-        $perm_templ_perm_edit = self::verifyPermission($this->db, 'templ_perm_edit');
+        $perm_edit_user_templ = self::verifyPermission($this->db, 'user_edit_templ_perm');
         $perm_is_godlike = self::verifyPermission($this->db, 'user_is_ueberuser');
 
         if (($details['uid'] == $_SESSION["userid"] && $perm_edit_own) || ($details['uid'] != $_SESSION["userid"] && $perm_edit_others)) {
@@ -1106,7 +1106,7 @@ class UserManager
                 active = :active";
 
             // If the user is allowed to change the permission template, set it.
-            if ($perm_templ_perm_edit == "1") {
+            if ($perm_edit_user_templ == "1") {
                 $templateError = (int)$details['templ_id'] === (int)$userCheck['perm_templ']
                     ? null
                     : $this->permissionTemplateAssignmentError(
@@ -1151,7 +1151,7 @@ class UserManager
             $stmt->bindValue(':email', $details['email'], PDO::PARAM_STR);
             $stmt->bindValue(':active', $active, PDO::PARAM_INT);
 
-            if ($perm_templ_perm_edit == "1") {
+            if ($perm_edit_user_templ == "1") {
                 $stmt->bindValue(':templ_id', $details['templ_id'], PDO::PARAM_INT);
             }
             if ($perm_is_godlike == "1") {
